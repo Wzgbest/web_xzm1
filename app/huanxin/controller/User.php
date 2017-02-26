5008473b3c0462c22b9ee6ac6a7787425d3698d8
wqw <wqw@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-02-26 03:48:47 +0000

    上传图片

--- app/huanxin/controller/User.php
+++ app/huanxin/controller/User.php
@@ -11,29 +11,31 @@ use app\huanxin\model\Employer;
 
 class User
 {
+    protected $employM;
+
     /**
      * 根据用户id,access_token 获取所有用户列表
      * @return string {"status":true/false,"message":"","friendsInfo":[]}
      *  friendsInfo=>["telephone"] => string(11) "13322223333"
-            ["userpic"] => string(53) "http://mat1.gtimg.com/www/images/qq2012/qqlogo_1x.png"
-            ["nickname"] => string(31) "zhongxun_xiaoshou_jack_nickname"
-            ["occupation"] => string(1) "6"
-            ["structid"] => string(12) "销售一部"
+     * ["userpic"] => string(53) "http://mat1.gtimg.com/www/images/qq2012/qqlogo_1x.png"
+     * ["nickname"] => string(31) "zhongxun_xiaoshou_jack_nickname"
+     * ["occupation"] => string(1) "6"
+     * ["structid"] => string(12) "销售一部"
      */
     public function getFriendsInfo()
     {
         $userid = input('param.userid');
         $access_token = input('param.access_token');
-        $chk_info = $this->checkUserAccess($userid,$access_token);
+        $chk_info = $this->checkUserAccess($userid, $access_token);
         if (!$chk_info['status']) {
             return $chk_info;
         }
-        $friendsInfo = $employM->getAllUsers();
-        $friendsInfo = get_struct_name($friendsInfo,$corp_id);
+        $friendsInfo = $this->employM->getAllUsers();
+        $friendsInfo = get_struct_name($friendsInfo, $corp_id);
         $info['message'] = 'SUCCESS';
         $info['status'] = true;
         $info['friendsInfo'] = $friendsInfo;
-        return json_encode($info,true);
+        return json_encode($info, true);
     }
 
     /**
@@ -44,13 +46,13 @@ class User
     {
         $userid = input('param.userid');
         $access_token = input('param.access_token');
-        $chk_info = $this->checkUserAccess($userid,$access_token);
+        $chk_info = $this->checkUserAccess($userid, $access_token);
         if (!$chk_info['status']) {
             return $chk_info;
         }
-        $friends_tel=$employM->getAllTels();
-        $info=['status' => true,'message'=>'SUCCESS','friendsTel'=>$friends_tel];
-        return json_encode($info,true);
+        $friends_tel = $this->employM->getAllTels();
+        $info = ['status' => true, 'message' => 'SUCCESS', 'friendsTel' => $friends_tel];
+        return json_encode($info, true);
     }
 
     /**
@@ -62,23 +64,23 @@ class User
         $userid = input('param.userid');
         if (!check_tel($userid)) {
             $info['message'] = '手机号码格式不正确';
-            return json_encode($info,true);
+            return json_encode($info, true);
         }
-        $code = rand(100000,999999);
-        $content = '【咕果】感谢您使用本产品，您的手机验证码为'.$code.'请及时填写';
-        $res = send_sms($userid,$code,$content);
+        $code = rand(100000, 999999);
+        $content = '【咕果】感谢您使用本产品，您的手机验证码为' . $code . '请及时填写';
+        $res = send_sms($userid, $code, $content);
         if ($res['status'] == false) {
             $info['message'] = $res['message'];
-            return json_encode($info,true);
+            return json_encode($info, true);
         }
         $info['message'] = '发送成功';
         $info['status'] = true;
-        return json_encode($info,true);
+        return json_encode($info, true);
     }
 
     /**
      * 验证码校验，更改密码
-     * @return {"status":true/false,"message":""}
+     * @return string {"status":true/false,"message":""}
      */
     public function resetPassword()
     {
@@ -88,32 +90,47 @@ class User
         $info['status'] = false;
         if (!check_tel($userid)) {
             $info['message'] = '手机号码格式不正确';
-            return json_encode($info,true);
+            return json_encode($info, true);
         }
-        $ini_code = session('reset_code'.$userid);
+        $ini_code = session('reset_code' . $userid);
         if ($ini_code != $code) {
             $info['message'] = '手机验证码不正确';
-            return json_encode($info,true);
+            return json_encode($info, true);
         }
         $data['password'] = md5($newpass);
-        $corp_id=get_corpid($userid);
+        $corp_id = get_corpid($userid);
         $employer = new Employer($corp_id);
-        $res = $employer->setEmployerSingleInfo($userid,$data);
+        $res = $employer->setEmployerSingleInfo($userid, $data);
         $info['status'] = true;
         $info['message'] = '修改成功，请重新登陆';
-        return json_encode($info,true);
+        return json_encode($info, true);
     }
 
+    /**
+     * 修改app头像
+     * @return array|mixed|string
+     */
     public function modifyUserPic()
     {
         $userid = input('param.userid');
         $access_token = input('param.access_token');
         $user_pic = input('param.userpic');
-        $chk_info = $this->checkUserAccess($userid,$access_token);
+        $info['status'] = false;
+        if ($user_pic == '') {
+            $info['message'] = '未设置头像';
+            return json_encode($info,true);
+        }
+        $chk_info = $this->checkUserAccess($userid, $access_token);
         if (!$chk_info['status']) {
             return $chk_info;
         }
-
+        $img_path = get_app_img($user_pic);
+        if (false ==$img_path['status']) {
+            return $img_path;
+        }
+        $data = ['userpic'=>$img_path['imgurl']];
+        $res = $this->employM->setEmployerSingleInfo($userid,$data);
+        return json_encode($img_path,true);
     }
 
     /**
@@ -122,14 +139,14 @@ class User
      * @param $access_token
      * @return array
      */
-    protected function checkUserAccess($userid,$access_token)
+    protected function checkUserAccess($userid, $access_token)
     {
         $info['status'] = false;
         if (empty($userid) || empty($access_token)) {
             $info['message'] = '用户id为空或token为空';
             return $info;
         }
-        if (!check_tel ($userid)) {
+        if (!check_tel($userid)) {
             $info['message'] = '用户名格式不正确';
             return $info;
         }
@@ -138,10 +155,10 @@ class User
             $info['message'] = '用户不存在';
             return $info;
         }
-        $employM = new Employer($corp_id);
-        $userinfo = $employM->getEmployer($userid);
+        $this->employM = new Employer($corp_id);
+        $userinfo = $this->employM->getEmployer($userid);
         if ($userinfo['system_token'] != $access_token) {
-            $info['message'] ='token不正确，请重新登陆';
+            $info['message'] = 'token不正确，请重新登陆';
             return $info;
         }
         $info['message'] = 'SUCCESS';
