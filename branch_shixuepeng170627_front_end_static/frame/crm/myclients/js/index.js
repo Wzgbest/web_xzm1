2d06240987198e9c0d53471f1486142f60d2f343
shixuepeng <shixuepeng@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-06-27 02:08:58 +0000

    myclient js

--- branch_shixuepeng170627_front_end_static/frame/crm/myclients/js/index.js
+++ branch_shixuepeng170627_front_end_static/frame/crm/myclients/js/index.js
@@ -1,11 +1,113 @@
-$("ul.firNav li").click(function(){
-	$("ul.firNav li").removeClass("current");
+$(".m-firNav li").click(function(){
+	$(".m-firNav li").removeClass("current");
 	$(this).addClass("current");
 });
-var content = "<ul class='tab-list'><li class='tab-1-ver'><input type='checkbox'/></li><li class='tab-2-ver'></li><li class='tab-3-ver'></li><li class='tab-4-ver'></li><li class='tab-5-ver'></li><li class='tab-6-ver'></li><li class='tab-7-ver'></li><li class='tab-8-ver'></li><li class='tab-9-ver'></li><li class='tab-10-ver'></li><li class='tab-11-ver'></li><li class='tab-12-ver'></li><li class='tab-13-ver'></li><li class='tab-14-ver'></li><li class='tab-15-ver'></li><li class='tab-16-ver'></li><div class='clearfix'></div></ul>";
-//$(".table").append(content);
+var content1 = "<ul class='u-tabList'><li class='u-tabCheckbox'><input type='checkbox'/></li><li class='u-tabCilentName'></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li class='u-tabLinkWay'></li><li></li><li></li><li></li><li></li><li></li><li class='u-tabOperation'><span>详情</span><span>编辑</span><span>释放</span></li><div class='clearfix'></div></ul>";
+var content2 = "<ul class='u-tabList'><li class='u-tabCheckbox'><input type='checkbox'/></li><li class='u-tabCilentName'></li><li></li><li></li><li></li><li></li><li></li><li></li><li class='u-tabLinkWay'></li><li></li><li></li><li></li><li class='u-tabOperation'><span>详情</span><span>编辑</span><span>释放</span></li><div class='clearfix'></div></ul>";
+
 var t;
-t=10;
+t=5;
 for(var i=0;i<t;i++){
-	$(".table").append(content);
+	$(".table1").append(content1);
+	$(".table2").append(content2);
+}
+function listNumChange(){
+	console.log($(".u-tabControlRow select").val());
+	t = $(".u-tabControlRow select").val();
+	$(".u-tabList").remove();
+	for(var i=0;i<t;i++){
+		$(".table1").append(content1);
+		$(".table2").append(content2);
+//		#myAllClientsPage
+	}
+//		var height = $("#myAllClientsPage").height();
+//		$("#myAllClientsPage").height(260+t*40);
+}
+/***************************/
+$("#blackBg").height(window.innerHeight);
+/*****************************************************************/
+/*新建*/
+function newClient(){
+	document.getElementById("newClient").classList.remove("hide");
+	document.getElementById("blackBg").classList.remove("hide");
+//	document.getElementsByTagName("body")[0].classList.add("hiddenY");
+}
+function removeNewClient(){
+	document.getElementById("newClient").classList.add("hide");
+	document.getElementById("blackBg").classList.add("hide");
+}
+
+/**********************************/
+//详情
+//$(".page-client").hide();
+//	$(".page-info").show();
+	
+/*$(".page-info").hide();
+$(".tab-list .tab-operation").eq(0).click(function(){
+	$(".page-client").hide();
+	$(".page-info").show();
+});*/
+/*焦点获取失去的交互*/
+/*$(".m-form input").focus(function(){
+	$(this).addClass("focus");
+});
+$(".m-form input").blur(function(){
+	$(this).removeClass("focus");
+});
+$(".m-form textarea").focus(function(){
+	$(this).addClass("focus");
+});
+$(".m-form textarea").blur(function(){
+	$(this).removeClass("focus");
+});*/
+/*客户详情——客户信息——编辑*/
+$("#clientInformation .edit i").click(function(){
+	$("#clientInformation").addClass("hide");
+	$("#clientInformationEdit").removeClass("hide");
+});
+/*客户详情-联系人-新建联系人*/
+$("#new-linkman").click(function(){
+	$("#creat-linkman").removeClass("hide");
+});
+$("#creat-linkman .close").click(function(){
+	$("#creat-linkman").addClass("hide");
+});
+function fun1(){
+	console.log($("#fff").val());
+	var val=$("#fff").val();
+	console.log(val);
+	if(val==1){
+		$(".sale-chance").addClass("hide");
+		$(".sale-chance-intentional").removeClass("hide");
+	}else if(val==2){
+		$(".sale-chance").addClass("hide");
+		$(".sale-chance-visit").removeClass("hide");
+	}else if(val==3){
+		$(".sale-chance").addClass("hide");
+		$(".sale-chance-finish").removeClass("hide");
+	}
+}
+$("#new-sale-chance").click(function(){
+	$("#creat-sale-chance").removeClass("hide");
+});
+/*跟踪记录*/
+$("#new-trace").click(function(){
+	$("#creat-traceRecord").removeClass("hide");
+});
+/*新建翻页*/
+function next1(){
+	$("#form1").addClass("hide");
+	$("#form2").removeClass("hide");
+}
+function next2(){
+	$("#form2").addClass("hide");
+	$("#form3").removeClass("hide");
+}
+function pre1(){
+	$("#form2").addClass("hide");
+	$("#form1").removeClass("hide");
+}
+function pre2(){
+	$("#form3").addClass("hide");
+	$("#form2").removeClass("hide");
 }
