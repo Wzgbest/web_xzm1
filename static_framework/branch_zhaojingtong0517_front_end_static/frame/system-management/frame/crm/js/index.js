b3409d75e541209ab8707c318de11de08d395498
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-06-30 02:20:57 +0000

    make branches static framework

---
+++ static_framework/branch_zhaojingtong0517_front_end_static/frame/system-management/frame/crm/js/index.js
@@ -0,0 +1,99 @@
+$(".m-firNav li").click(function(){
+	$(".m-firNav li").removeClass("current");
+	$(this).addClass("current");
+});
+var content1 = "<ul class='u-tabList'><li ></li><li ></li><li class='u-tabOperation'><span>查看员工</span><span>移除权限</span></li><div class='clearfix'></div></ul>";
+var content2 = "<ul class='u-tabList'><li class='u-tabCheckbox'><input type='checkbox'/></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li class='u-tabOperation'><span>详情</span><span>编辑</span><span>删除</span></li><div class='clearfix'></div></ul>";
+var content3 = "<ul class='u-tabList'><li></li><li></li><li></li><li></li><li></li><li></li><li class='u-tabOperation'><span>详情</span><span class='big-link' data-reveal-id='myModal' data-animation='fade'>移至</span></li><div class='clearfix'></div></ul>";
+
+var t;
+t=15;
+for(var i=0;i<t;i++){
+	$(".table1").append(content1);
+	$(".table2").append(content2);
+	$(".table3").append(content3);
+}
+function listNumChange(){
+	console.log($(".u-tabControlRow select").val());
+	t = $(".u-tabControlRow select").val();
+	$(".u-tabList").remove();
+	for(var i=0;i<t;i++){
+		$(".table1").append(content1);
+		$(".table2").append(content2);
+		$(".table3").append(content3);
+//		#myAllClientsPage
+	}
+//		var height = $("#myAllClientsPage").height();
+//		$("#myAllClientsPage").height(260+t*40);
+}
+/***************************/
+$("#blackBg").height(window.innerHeight);
+/*****************************************************************/
+/*新建*/
+function newClient(){
+	document.getElementById("newClient").classList.remove("hide");
+	document.getElementById("blackBg").classList.remove("hide");
+//	document.getElementsByTagName("body")[0].classList.add("hiddenY");
+}
+function removeNewClient(){
+	document.getElementById("newClient").classList.add("hide");
+	document.getElementById("blackBg").classList.add("hide");
+}
+
+/**********************************/
+//详情
+//$(".page-client").hide();
+//	$(".page-info").show();
+	
+/*$(".page-info").hide();
+$(".tab-list .tab-operation").eq(0).click(function(){
+	$(".page-client").hide();
+	$(".page-info").show();
+});*/
+/*焦点获取失去的交互*/
+$(".m-form input").focus(function(){
+	$(this).addClass("focus");
+});
+$(".m-form input").blur(function(){
+	$(this).removeClass("focus");
+});
+$(".m-form textarea").focus(function(){
+	$(this).addClass("focus");
+});
+$(".m-form textarea").blur(function(){
+	$(this).removeClass("focus");
+});
+/*客户详情——客户信息——编辑*/
+$("#clientInformation .edit i").click(function(){
+	$("#clientInformation").addClass("hide");
+	$("#clientInformationEdit").removeClass("hide");
+});
+/*客户详情-联系人-新建联系人*/
+$("#new-linkman").click(function(){
+	$("#creat-linkman").removeClass("hide");
+});
+$("#creat-linkman .close").click(function(){
+	$("#creat-linkman").addClass("hide");
+});
+function fun1(){
+	console.log($("#fff").val());
+	var val=$("#fff").val();
+	console.log(val);
+	if(val==1){
+		$(".sale-chance").addClass("hide");
+		$(".sale-chance-intentional").removeClass("hide");
+	}else if(val==2){
+		$(".sale-chance").addClass("hide");
+		$(".sale-chance-visit").removeClass("hide");
+	}else if(val==3){
+		$(".sale-chance").addClass("hide");
+		$(".sale-chance-finish").removeClass("hide");
+	}
+}
+$("#new-sale-chance").click(function(){
+	$("#creat-sale-chance").removeClass("hide");
+});
+/*跟踪记录*/
+$("#new-trace").click(function(){
+	$("#creat-traceRecord").removeClass("hide");
+});
