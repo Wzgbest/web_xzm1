cbda18112d26ba757250169f60c55f8da3d05f59
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-07-06 08:59:27 +0000

    合并代码自cxx

--- trunk/app/knowledgebase/controller/CorporationShare.php
+++ trunk/app/knowledgebase/controller/CorporationShare.php
@@ -8,24 +8,25 @@
 // +----------------------------------------------------------------------
 namespace app\knowledgebase\controller;
 
-use app\huanxin\controller\User;
+use app\common\controller\Initialize;
 use app\knowledgebase\model\CorporationShare as CorporationShareModel;
+use app\knowledgebase\model\CorporationShareComment as CorporationShareCommentModel;
 use app\knowledgebase\model\CorporationSharePicture;
 
-class CorporationShare{
-    public function addShare(User $user){
-        $userid = input('param.userid');
-        $access_token = input('param.access_token');
-        $chk_info = $user->checkUserAccess($userid,$access_token);
-        if (!$chk_info['status']) {
-            return json($chk_info);
-        }
+class CorporationShare extends Initialize{
+    var $paginate_list_rows = 10;
+    public function _initialize(){
+        parent::_initialize();
+        $this->paginate_list_rows = config("paginate.list_rows");
+    }
+    public function addShare(){
+        $userinfo = get_userinfo();
+        $uid = $userinfo["userid"];
 
         $result = ['status'=>0 ,'info'=>"发布动态时发生错误！"];
         $msg = input('param.msg');
         if(!$msg){
-            $result['info'] = "参数错误！";
-            return json($result);
+            exception("参数错误!");
         }
         $imgs = request()->file('img');
         $img_num = 9;
@@ -35,7 +36,7 @@ class CorporationShare{
         //trace(var_exp($imgs,'$imgs','return'));
         $infos = [];
         if($imgs) {
-            $path = ROOT_PATH . 'public' . DS . 'webroot' . DS . $chk_info["corp_id"] . DS . 'images';
+            $path = ROOT_PATH . 'public' . DS . 'webroot' . DS . $this->corp_id . DS . 'images';
             foreach($imgs as $img){
                 $checkFlg = $img->check(["ext"=>config('upload_image.image_ext')]);
                 //trace(var_exp($img,'$img','return'));
@@ -53,10 +54,10 @@ class CorporationShare{
             }
         }
         //trace(var_exp($infos,'$infos','return'));
-        $share["userid"] = $chk_info['userinfo']['id'];
+        $share["userid"] = $uid;
         $share["content"] = $msg;
         $share["create_time"] = time();
-        $corporationShareModel = new CorporationShareModel($chk_info["corp_id"]);
+        $corporationShareModel = new CorporationShareModel($this->corp_id);
         $corporationShareModel->link->startTrans();
         $share_id = $corporationShareModel->createCorporationShare($share);
         //trace(var_exp($share_id,'$share_id','return'));
@@ -67,13 +68,13 @@ class CorporationShare{
         if($infos){
             $share_pictures = [];
             $share_picture["share_id"] = $share_id;
-            $url_path = DS . 'webroot' . DS . $chk_info["corp_id"] . DS . 'images' . DS;
+            $url_path = DS . 'webroot' . DS . $this->corp_id . DS . 'images' . DS;
             foreach ($infos as $info){
                 $share_picture["path"] = $url_path . $info;
                 $share_pictures[] = $share_picture;
             }
             //trace(var_exp($share_pictures,'$share_pictures','return'));
-            $corporationSharePicture = new CorporationSharePicture($chk_info["corp_id"]);
+            $corporationSharePicture = new CorporationSharePicture($this->corp_id);
             $corporationSharePicture->createMutipleCorporationSharePicture($share_pictures);
         }
         $corporationShareModel->link->commit();
@@ -82,26 +83,68 @@ class CorporationShare{
         $result['info'] = "发布成功！";
         return json($result);
     }
-    public function shareList(User $user){
-        $userid = input('param.userid');
-        $access_token = input('param.access_token');
-        $chk_info = $user->checkUserAccess($userid,$access_token);
-        if (!$chk_info['status']) {
-            return json($chk_info);
-        }
+    public function shareList(){
         $result = ['status'=>0 ,'info'=>"获取动态时发生错误！"];
         $num = input('num',10,'int');
         $last_id = input("last_id",0,"int");
-        $corporationShareModel = new CorporationShareModel($chk_info["corp_id"]);
+        $corporationShareModel = new CorporationShareModel($this->corp_id);
         $share_data = $corporationShareModel->getCorporationShare($num,$last_id);
+        $share_ids = array_column($share_data,"id");
+        $corporationShareCommentModel = new CorporationShareCommentModel($this->corp_id);
+        $share_comment_data = $corporationShareCommentModel->getCorporationShareComment($share_ids,$num,$last_id);
+        $share_comment_Index = [];
+        foreach ($share_comment_data as $share_comment){
+            $share_comment_Index[$share_comment["share_id"]][] = $share_comment;
+        }
+        foreach ($share_data as &$share){
+            if(isset($share_comment_Index[$share["id"]])){
+                $share["comments"] = $share_comment_Index[$share["id"]];
+            }
+        }
         $result['data'] = $share_data;
         $result['status'] = 1;
         $result['info'] = "获取成功！";
         return json($result);
     }
     public function shareInfo(){}
-    public function relayShare(){}
-    public function addComment(){}
+    public function relayShare(){
+
+    }
+    public function addComment(){
+        $result = ['status'=>0 ,'info'=>"评论动态时发生错误！"];
+        $share_id = input('share_id',0,"int");
+        $reply_content = input('reply_content',"","string");
+        if(empty($share_id) || empty($reply_content)){
+            exception("参数错误!");
+        }
+        $userinfo = get_userinfo();
+        $uid = $userinfo["userid"];
+        $replyer_id = $uid;
+        $reviewer = input('reviewer',0,"int");
+        $reviewer_id = 0;
+        if($reviewer){
+            if (!check_tel($reviewer)) {
+                exception("用户名格式不正确!");
+            }
+            $corp_id = get_corpid($reviewer);
+            if ($corp_id == false) {
+                exception("用户不存在!");
+            }
+            $this->employM = new Employee($this->corp_id);
+            $reviewer_info = $this->employM->getEmployeeByTel($reviewer);
+            $reviewer_id = $reviewer_info["id"];
+        }
+        $comment["share_id"] = $share_id;
+        $comment["replyer_id"] = $replyer_id;
+        $comment["reply_content"] = $reply_content;
+        $comment["reviewer_id"] = $reviewer_id;
+        $corporationShareCommentModel = new CorporationShareCommentModel($this->corp_id);
+        $add_comment_flg= $corporationShareCommentModel->createCorporationShareComment($comment);
+        $result['data'] = $add_comment_flg;
+        $result['status'] = 1;
+        $result['info'] = "评论成功！";
+        return json($result);
+    }
     public function like(){}
     public function tip(){}
 }
--- trunk/app/knowledgebase/model/CorporationSharePicture.php
+++ trunk/app/knowledgebase/model/CorporationSharePicture.php
@@ -20,65 +20,6 @@ class CorporationSharePicture extends Base{
     }
 
     /**
-     * 获取配图
-     * @param $uid int 用户id
-     * @param $map array 配图筛选条件
-     * @param $order string 排序
-     * @return array
-     * @throws \think\Exception
-     */
-    public function getAllCorporationSharePicture($uid,$map=null,$order="id desc"){
-        $map["create_user"] = $uid;
-        $map["status"]=1;
-        $searchCustomerList = $this->model
-            ->table($this->table)
-            ->where($map)
-            ->order($order)
-            ->field(array("id","customer_name","phone","contact_name","industry","com_adds","website"))
-            ->select();
-        return ['res'=>$searchCustomerList ,'error'=>"0"];
-    }
-
-    /**
-     * 获取配图
-     * @param $num int 数量
-     * @param $page int 页
-     * @param $map array 配图筛选条件
-     * @param $order string 排序
-     * @return array
-     * @throws \think\Exception
-     */
-    public function getCorporationSharePicture($num=10,$page=0,$map=null,$order="id desc"){
-        $offset = 0;
-        if($page){
-            $offset = ($page-1)*$num;
-        }
-        $searchCustomerList = $this->model
-            ->table($this->table)
-            ->where($map)
-            ->order($order)
-            ->limit($offset,$num)
-            ->field("*")//TODO
-            ->select();
-        return ['res'=>$searchCustomerList ,'error'=>"0"];
-    }
-
-    /**
-     * 创建配图,并返回结果
-     * @param $picture array 配图信息
-     * @return array
-     * @throws \think\Exception
-     */
-    public function createCorporationSharePicture($picture){
-        $share_id = $picture['share_id'];
-        if(empty($share_id)){
-            return ['res'=>0 ,'error'=>"1" ,'msg'=>"参数错误！"];
-        }
-        $b = $this->model->table($this->table)->insertGetId($picture);
-        return ['res'=>$b ,'error'=>"0"];
-    }
-
-    /**
      * 创建配图,并返回结果
      * @param $pictures array 配图信息数组
      * @return array
@@ -86,35 +27,6 @@ class CorporationSharePicture extends Base{
      */
     public function createMutipleCorporationSharePicture($pictures){
         $b = $this->model->table($this->table)->insertAll($pictures);
-        return ['res'=>$b ,'error'=>"0"];
-    }
-
-    /**
-     * 更新配图,并返回结果
-     * @param $picture array 配图信息
-     * @param $map array 配图筛选条件
-     * @return array
-     * @throws \think\Exception
-     */
-    public function updateCorporationSharePicture($picture,$map){
-        if(empty($map)){
-            return ['res'=>0 ,'error'=>"1" ,'msg'=>"更新参数错误！"];
-        }
-        $b = $this->model->table($this->table)->where($map)->data($picture)->save();
-        return ['res'=>$b ,'error'=>"0"];
-    }
-
-    /**
-     * 删除配图,并返回结果
-     * @param $map array 配图筛选条件
-     * @return array
-     * @throws \think\Exception
-     */
-    public function delCorporationSharePicture($map){
-        if(empty($map)){
-            return ['res'=>0 ,'error'=>"1" ,'msg'=>"缺少删除目标！"];
-        }
-        $b = $this->model->table($this->table)->where($map)->delete();
         return ['res'=>$b ,'error'=>"0"];
     }
 }
