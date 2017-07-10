54422ba37ee708dab7b17df3e042ffa100026447
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-07-10 02:05:36 +0000

    部门树改造

--- branch_caoxinxin20170405/public/static/js/tree.js
+++ branch_caoxinxin20170405/public/static/js/tree.js
@@ -32,31 +32,31 @@ function tree(config) {
 
     //get html by tree
     this.get_html=function(node){
-        var html = '<div class="five_tree">';
+        var html = '';
         var lenght = node.length;
         for(var i=0;i<lenght;i++){
             var sub_node = node[i];
             var class_str = '';
             var child_str = '';
-            if(i==lenght){
+            if(i+1==lenght){
                 class_str+=" is_last";
             }
-            html+='<div class="node node'+sub_node["id"];
-            html+=class_str+'" node_id="'+sub_node["id"]+'">';
-            html+='<div class="node_item">';
             if(sub_node.hasOwnProperty("child")){
                 child_str+='<div class="child_list child_list'+sub_node["id"]+' hide">';
                 child_str+=self.get_html(sub_node["child"]);
                 child_str+='</div>';
-                html+="<i class='node_plus'>+</i>";
+            }else{
+                class_str+=" is_leaf";
             }
+            html+='<div class="node node'+sub_node["id"];
+            html+=class_str+'" node_id="'+sub_node["id"]+'">';
+            html+='<div class="node_item">';
             html+='<span class="node_name">'+sub_node["struct_name"]+'</span>';
-            html+='<img class="node_tool add" src="/systemsetting/images/add.png" />';
+            html+='<img class="node_tool del" src="/systemsetting/images/del.png" />';
             html+='<img class="node_tool info" src="/systemsetting/images/compile.png" />';
-            html+='<img class="node_tool del" src="/systemsetting/images/del.png" /></div>';
-            html+=child_str+'</div>';
+            html+='<img class="node_tool add" src="/systemsetting/images/add.png" />';
+            html+='</div>'+child_str+'</div>';
         }
-        html+='</div>';
         return html;
     };
 
@@ -67,37 +67,44 @@ function tree(config) {
         }
     };
     this.update=function(){
-        this.tree_html = this.get_html(this.data,1);
+        this.tree_html = '<div class="five_tree">'+this.get_html(this.data,1)+'</div>';
         $(this.target).html(this.tree_html);
     };
     this.update();
-    $(this.target).on('click','.node_plus',function(){
-        $(this).parent().parent().children(".child_list").toggleClass('hide');
+    $(this.target).on('click','.node_item',function(){
+        var is_plus = !$(this).hasClass("node_sub");
+        if(is_plus){
+            $(this).parent().children(".child_list").removeClass('hide');
+            $(this).addClass('node_sub');
+        }else{
+            $(this).parent().children(".child_list").addClass('hide');
+            $(this).removeClass('node_sub');
+        }
     });
     $(this.target).on('click','.node_name',function(){
         if(self.listen_arr.selFun!=null){
-            var id = $(this).parent().parent().attr("node_id");
+            var id = $(this).parent().attr("node_id");
             console.log("sel",id);
             self.listen_arr.selFun(id);
         }
     });
     $(this.target).on('click','.node .add',function(){
         if(self.listen_arr.addFun!=null){
-            var id = $(this).parent().parent().attr("node_id");
+            var id = $(this).parent().attr("node_id");
             console.log("add",id);
             self.listen_arr.addFun(id);
         }
     });
     $(this.target).on('click','.node .info',function(){
         if(self.listen_arr.editFun!=null){
-            var id = $(this).parent().parent().attr("node_id");
+            var id = $(this).parent().attr("node_id");
             console.log("info",id);
             self.listen_arr.editFun(id);
         }
     });
     $(this.target).on('click','.node .del',function(){
         if(self.listen_arr.delFun!=null){
-            var id = $(this).parent().parent().attr("node_id");
+            var id = $(this).parent().attr("node_id");
             console.log("del",id);
             self.listen_arr.delFun(id);
         }
