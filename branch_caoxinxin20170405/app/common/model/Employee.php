117ccbb0eefb5c13328c44ab75d6129549363d55
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-07-17 02:58:00 +0000

    列表实例化错误

--- branch_caoxinxin20170405/app/common/model/Employee.php
+++ branch_caoxinxin20170405/app/common/model/Employee.php
@@ -433,7 +433,7 @@ class Employee extends Base{
             ->field('e.id as user_id,e.truename,e.worknum,e.telephone,e.email,e.create_time,e.is_leader,GROUP_CONCAT( distinct re.role_id) as role,GROUP_CONCAT( distinct r.role_name) as role_name,GROUP_CONCAT( distinct se.struct_id) as struct,GROUP_CONCAT( distinct ses.struct_id) as struct_id,GROUP_CONCAT( distinct s.struct_name) as struct_name')
             ->where('se.struct_id',$struct_id)
             ->group("e.id");
-        if (is_null($rows)) {
+        if (!is_null($rows)) {
             $query = $query->limit($page,$rows);
         }
         $employee_list = $query->select();
