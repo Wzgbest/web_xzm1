b616f15268fc3dd21037a6f9ab7a3f000cdb199e
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-07-18 03:53:35 +0000

    合并代码自cxx

--- trunk/app/systemsetting/controller/BusinessFlow.php
+++ trunk/app/systemsetting/controller/BusinessFlow.php
@@ -4,10 +4,9 @@ namespace app\systemsetting\controller;
 use app\common\controller\Initialize;
 use app\common\model\Business;
 
-class BusinessFlow extends Initialize
-{
-    public function index()
-    {
+class BusinessFlow extends Initialize{
+    public function index()    {
         $business_flow = new Business();
+        return view();
     }
 }
