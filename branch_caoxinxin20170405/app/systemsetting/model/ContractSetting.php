1a8d34b99055f49fc8f9342e7e823dcc201890ac
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-07-19 01:52:06 +0000

    完善客户设置,发票设置添加页面

--- branch_caoxinxin20170405/app/systemsetting/model/ContractSetting.php
+++ branch_caoxinxin20170405/app/systemsetting/model/ContractSetting.php
@@ -14,7 +14,7 @@ class ContractSetting extends Base{
     protected $dbprefix;
     public function __construct(){
         $this->dbprefix = config('database.prefix');
-        $this->table = $this->dbprefix.'customer_setting';
+        $this->table = $this->dbprefix.'contract_setting';
         parent::__construct();
     }
 
@@ -25,5 +25,67 @@ class ContractSetting extends Base{
             ->field("*")
             ->select();
         return $contractSettingList;
+    }
+    /**
+     * 查询合同设置
+     * @param $num int 数量
+     * @param $page int 页
+     * @param $map array 合同筛选条件
+     * @param $order string 排序
+     * @return array|false
+     * @throws \think\Exception
+     */
+    public function getContractSetting($num=10,$page=0,$map=null,$order="id desc"){
+        $offset = 0;
+        if($page){
+            $offset = ($page-1)*$num;
+        }
+        $contractSettingList = $this->model
+            ->table($this->table)
+            ->where($map)
+            ->order($order)
+            ->limit($offset,$num)
+            ->field('*')//TODO field list
+            ->select();
+        if($num==1&&$page==0&&$contractSettingList){
+            $contractSettingList = $contractSettingList[0];
+        }
+        return $contractSettingList;
+    }
+
+    /**
+     * 添加单个合同设置
+     * @param $data
+     * @return int|string
+     */
+    public function addContractSetting($data)
+    {
+        return $this->model->table($this->table)->insertGetId($data);
+    }
+
+    /**
+     * 根据合同设置id修改合同设置
+     * @param $id
+     * @param $data
+     * @return int|string
+     * @throws \think\Exception
+     */
+    public function setContractSetting($id,$data)
+    {
+        return $this->model->table($this->table)->where('id',$id)->update($data);
+    }
+
+    /**
+     * 删除合同设置,并返回结果
+     * @param $map array 合同筛选条件
+     * @return array
+     * @throws \think\Exception
+     */
+    public function delContractSetting($map){
+        if(empty($map)){
+            return ['res'=>0 ,'error'=>"1" ,'msg'=>"缺少删除目标！"];
+        }
+        $b = $this->model->table($this->table)->where($map)->delete();
+        return ['res'=>$b ,'error'=>"0"];
     }
 }
