000556dd635dd380c303d32b77d15b6f4db37420
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-07-20 02:35:41 +0000

    红包直接使用数据库,增加查询交易记录

--- branch_caoxinxin20170405/app/huanxin/model/TakeCash.php
+++ branch_caoxinxin20170405/app/huanxin/model/TakeCash.php
@@ -25,4 +25,44 @@ class TakeCash extends Base
     {
         return $this->model->table($this->table)->insert($data);
     }
+
+    /**
+     * 查询交易信息
+     * @param $user
+     * @return int|string
+     */
+    public function getOrderByPage($user,$page=1,$num=10){
+        $map["userid"] = $user;
+        $query = $this->model->table($this->table)
+            ->where($map)
+            ->order("id desc");
+        if($num){
+            //分页
+            $offset = 0;
+            if($page){
+                $offset = ($page-1)*$num;
+            }
+            $query->limit($offset,$num);
+        }
+        $order_list = $query->select();
+        return $order_list;
+    }
+
+    /**
+     * 查询交易信息
+     * @param $user
+     * @return int|string
+     */
+    public function getOrderList($user,$num=10,$last_id=0){
+        $map["userid"] = $user;
+        if($last_id){
+            $map["id"] = ["lt",$last_id];
+        }
+        $order_list = $this->model->table($this->table)
+            ->where($map)
+            ->order("id desc")
+            ->limit($num)
+            ->select();
+        return $order_list;
+    }
 }
