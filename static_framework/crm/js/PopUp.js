4952978766e40b63084bc9f0fc25dfc67b7ec751
shixuepeng <shixuepeng@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-07-29 03:24:33 +0000

    合同申请  +  弹窗js改写

--- static_framework/crm/js/PopUp.js
+++ static_framework/crm/js/PopUp.js
@@ -49,3 +49,23 @@ function removePop(){
 	document.getElementById("popUpContent").remove();
 	blackBghide();
 }
+function pop(id,url,clicker){
+    $(clicker).click(function(){
+    	$(id).removeClass("hide");
+    });
+    $(document).on('click', id + " header h1 i", function() {
+	   $(id).addClass("hide");
+	});
+    $.ajax({
+        url: url,
+        type: 'get',
+        async: false,
+        success: function(data) {
+            $(id).html(data);
+        },
+        error: function() {
+            alert("简报加载失败!");
+        }
+    });
+}
+
