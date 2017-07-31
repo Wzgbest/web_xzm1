8692994bee9edd7457f33a78114714e2737c47fe
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-07-31 04:43:13 +0000

    合同页面资源

--- branch_caoxinxin20170405/app/crm/controller/Contract.php
+++ branch_caoxinxin20170405/app/crm/controller/Contract.php
@@ -1,9 +1,35 @@
 <?php
 namespace app\crm\controller;
 
-class Contract
-{
+class Contract{
     public function index(){
         return view();
+    }
+    public function contract_apply(){
+        return view();
+    }
+    public function apply(){
+        $result = ['status'=>0 ,'info'=>"申请合同时发生错误！"];
+        $result['status']=1;
+        $result['info']='申请合同开发中!';
+        return $result;
+    }
+    public function retract(){
+        $result = ['status'=>0 ,'info'=>"撤回合同申请时发生错误！"];
+        $result['status']=1;
+        $result['info']='撤回合同申请开发中!';
+        return $result;
+    }
+    public function approved(){
+        $result = ['status'=>0 ,'info'=>"通过合同申请时发生错误！"];
+        $result['status']=1;
+        $result['info']='通过合同申请开发中!';
+        return $result;
+    }
+    public function rejected(){
+        $result = ['status'=>0 ,'info'=>"驳回合同申请时发生错误！"];
+        $result['status']=1;
+        $result['info']='驳回合同申请开发中!';
+        return $result;
     }
 }
