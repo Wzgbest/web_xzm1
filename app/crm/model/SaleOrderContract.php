9cfadffeef58562c9dec968d8c8de2dc1e47a5f9
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-08-01 06:30:48 +0000

    合并代码自cxx

--- app/crm/model/SaleOrderContract.php
+++ app/crm/model/SaleOrderContract.php
@@ -10,60 +10,60 @@ namespace app\crm\model;
 
 use app\common\model\Base;
 
-class SaleChanceVisit extends Base{
+class SaleOrderContract extends Base{
     protected $dbprefix;
     public function __construct($corp_id){
         $this->dbprefix = config('database.prefix');
-        $this->table = config('database.prefix').'sale_chance_visit';
+        $this->table = config('database.prefix').'sale_order_contract';
         parent::__construct($corp_id);
     }
 
-    /**根据商机ID获取客户商机拜访
+    /**根据商机ID获取销售单
      * @param $sale_id int 商机id
      * @return false|\PDOStatement|int|\think\Collection
      * created by blu10ph
      */
-    public function getSaleChanceVisitBySaleId($sale_id)
+    public function getSaleOrderContractBySaleId($sale_id)
     {
-        return $this->model->table($this->table)->alias('scv')
-            ->where('scv.sale_id',$sale_id)
+        return $this->model->table($this->table)->alias('soc')
+            ->where('soc.sale_id',$sale_id)
             ->find();
     }
 
     /**获取
-     * @param $id int 客户商机拜访id
+     * @param $id int 销售单id
      * @return false|\PDOStatement|int|\think\Collection
      * created by blu10ph
      */
-    public function getSaleChanceVisit($id)
+    public function getSaleOrderContract($id)
     {
         return $this->model->table($this->table)->where('id',$id)->find();
     }
 
     /**添加
-     * @param $data array 客户商机拜访数据
+     * @param $data array 销售单数据
      * @return false|\PDOStatement|int|\think\Collection
      * created by blu10ph
      */
-    public function addSaleChanceVisit($data)
+    public function addSaleOrderContract($data)
     {
         return $this->model->table($this->table)->insertGetId($data);
     }
 
     /**更新
-     * @param $id int 客户商机拜访id
-     * @param $data array 客户商机拜访数据
+     * @param $id int 销售单id
+     * @param $data array 销售单数据
      * @return false|\PDOStatement|int|\think\Collection
      * created by blu10ph
      */
-    public function setSaleChanceVisit($id,$data)
+    public function setSaleOrderContract($id,$data)
     {
         return $this->model->table($this->table)->where('id',$id)->update($data);
     }
 
     /**更新
      * @param $sale_id int 客户商机id
-     * @param $data array 客户商机拜访数据
+     * @param $data array 销售单数据
      * @return false|\PDOStatement|int|\think\Collection
      * created by blu10ph
      */
