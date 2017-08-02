1814725be468aba72bf5964f7f06a7497f5a1246
shixuepeng <shixuepeng@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-08-02 00:26:12 +0000

    合同申请+发票

--- static_framework/crm/js/PopUp.js
+++ static_framework/crm/js/PopUp.js
@@ -51,21 +51,21 @@ function removePop(){
 }
 function pop(id,url,clicker){
     $(clicker).click(function(){
-    	$(id).removeClass("hide");
+    	$.ajax({
+        	url: url,
+        	type: 'get',
+        	async: false,
+        	success: function(data) {
+            	$(id).html(data);
+            	$(id).removeClass("hide");
+        	},
+        	error: function() {
+            	alert("加载失败!");
+        	}
+    	});   	
     });
-    $(document).on('click', id + " header h1 i", function() {
+    $(document).on('click', id+" .pop-close-btn", function() {
 	   $(id).addClass("hide");
-	});
-    $.ajax({
-        url: url,
-        type: 'get',
-        async: false,
-        success: function(data) {
-            $(id).html(data);
-        },
-        error: function() {
-            alert("简报加载失败!");
-        }
-    });
+	}); 
 }
 
