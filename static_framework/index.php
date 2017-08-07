16aca1b98aebbf060c5782da141196ba824721c8
shixuepeng <shixuepeng@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-08-07 07:12:01 +0000

    url处理支持列切换

--- static_framework/index.php
+++ static_framework/index.php
@@ -7,14 +7,14 @@ if($_SERVER["REQUEST_URI"]=="/"){
 if(count($request_uri_array)>=4){
 	session_start();
 	$file_url = "./".$request_uri_array[1]."/".$request_uri_array[2];
+	$file_url = strtolower($file_url);
 	$file_array = explode(",",$request_uri_array[3]);
 	if(count($file_array)==2 && $file_array[1]=='html'){
-		$file_url .= "/".$request_uri_array[3];
+		$file_url .= "/".$file_array[0];
 	}else{
-		$file_url .= "/".$request_uri_array[3].".html";
+		$file_url .= "/".$request_uri_array[3];
 	}
-	$file_url = strtolower($file_url);
-	if($file_url=="./login/index/verifylogin.html"){
+	if($file_url=="./login/index/verifylogin"){
 		$_SESSION['login_flg']=1;
 		echo '<script language="javascript">top.location="/";</script>';
 		exit;
@@ -23,6 +23,15 @@ if(count($request_uri_array)>=4){
 		echo file_get_contents("./login/index/index.html");
 		exit;
 	}
+	if(in_array("in_column",$request_uri_array)){
+		$key = array_search("in_column",$request_uri_array);
+		if(isset($request_uri_array[$key+1])){
+			$val = intval($request_uri_array[$key+1]);
+			$file_url .= "_in_column_".$val;
+		}
+	}
+	$file_url.=".html";
+	//echo $file_url;exit();
 	if(file_exists($file_url)){
 		echo file_get_contents($file_url);
 		exit;
