d4cfc516a435c351deffe48145df2875ce853855
shixuepeng <shixuepeng@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-08-12 03:23:39 +0000

    修改了所有涉及listmanage的页面的结构，以及对应的css；审核页面重新构筑一下

--- static_framework/verification/js/index.js
+++ static_framework/verification/js/index.js
@@ -1,16 +1,16 @@
-$(".verification_index_index .index-record").click(function(){
-	$.ajax({
-        url: "/verification/index/record.html",
-        type: 'get',
-        async: false,
-        success: function(data) {
-            $('.index-record-page').html(data);
-        },
-        error: function() {
-           
-        }
-    });
-});
-$(document).on("click",".verification_record header ul li",function(){
-	$('.index-record-page').empty();
-});
+
+function recordShow(clicker,url,shower){
+	$(clicker+" .index-record").click(function(){
+			$.ajax({
+	        url: url,
+	        type: 'get',
+	        async: false,
+	        success: function(data) {
+	            $(shower).html(data);
+	        },
+	        error: function() {
+	           
+	        }
+	    });
+	});
+}
