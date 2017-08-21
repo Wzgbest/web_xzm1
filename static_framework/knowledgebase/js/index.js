04a05e70143537a2d963688a54cabfff0f2e0359
shixuepeng <shixuepeng@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-08-21 00:32:31 +0000

    知识库-话术库

---
+++ static_framework/knowledgebase/js/index.js
@@ -0,0 +1,6 @@
+$(".new-speechcraft").click(function(){
+	loadPage("/knowledgebase/speechcraft/new", "speechcraftfr");
+});
+$(".knowledgebase_speechcraft_new .radio-select input[name='class']").change(function(){
+	console.log(1);
+})
