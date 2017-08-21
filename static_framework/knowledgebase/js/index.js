8aa30090795ba6ef49beadfcb8e4cd01c5a28ac0
shixuepeng <shixuepeng@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-08-21 01:24:13 +0000

    小改动

--- static_framework/knowledgebase/js/index.js
+++ static_framework/knowledgebase/js/index.js
@@ -3,4 +3,11 @@ $(".new-speechcraft").click(function(){
 });
 $(".knowledgebase_speechcraft_new .radio-select input[name='class']").change(function(){
 	console.log(1);
+	if($(this).val()==1){
+		$("form.original").removeClass("hide");
+		$("form.quote").addClass("hide");
+	}else if($(this).val()==2){
+		$("form.original").addClass("hide");
+		$("form.quote").removeClass("hide");
+	}
 })
