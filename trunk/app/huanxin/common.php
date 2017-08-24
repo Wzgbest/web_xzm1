d2e8c0f4836d8b9ab743ffce0bc57d935959771b
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-08-24 06:18:48 +0000

    合并代码自cxx

--- trunk/app/huanxin/common.php
+++ trunk/app/huanxin/common.php
@@ -2,5 +2,5 @@
 use think\Hook;
 
 
-//红包过期检查
-Hook::add('check_over_time_red','app\\huanxin\\behavior\\CheckOverTimeRedEnvelope');
+//红包过期检查  已废弃,改用定时任务
+//Hook::add('check_over_time_red','app\\huanxin\\behavior\\CheckOverTimeRedEnvelope');
