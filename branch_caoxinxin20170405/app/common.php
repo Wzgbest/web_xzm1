2c7ecbbf34cd1b4546636a55c7a960f209b8fdd2
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-09-04 03:15:41 +0000

    变量名完善

--- branch_caoxinxin20170405/app/common.php
+++ branch_caoxinxin20170405/app/common.php
@@ -465,23 +465,23 @@ function getCommStatusName($comm_status){
     return $comm_status_name;
 }
 
-function getBelongsToManageName($resource_from){
-    $resource_from_name = null;
-    switch ($resource_from){
+function getBelongsToManageName($belongs_to){
+    $belongs_to_name = null;
+    switch ($belongs_to){
         case 1:
         case 2:
-        $resource_from_name = "未申领";
+        $belongs_to_name = "未申领";
             break;
         case 3:
-            $resource_from_name = "跟进中";
+            $belongs_to_name = "跟进中";
             break;
         case 4:
-            $resource_from_name = "待处理";
+            $belongs_to_name = "待处理";
             break;
         default:
-            $resource_from_name = "无";
+            $belongs_to_name = "无";
     }
-    return $resource_from_name;
+    return $belongs_to_name;
 }
 
 function getResourceFromName($resource_from){
@@ -501,22 +501,22 @@ function getResourceFromName($resource_from){
     }
     return $resource_from_name;
 }
-function getTakeTypeFromName($resource_from){
-    $resource_from_name = null;
-    switch ($resource_from){
+function getTakeTypeFromName($take_type){
+    $take_type_name = null;
+    switch ($take_type){
         case 1:
-            $resource_from_name = "转介绍";
+            $take_type_name = "转介绍";
             break;
         case 2:
-            $resource_from_name = "搜索";
+            $take_type_name = "搜索";
             break;
         case 3:
-            $resource_from_name = "购买";
+            $take_type_name = "购买";
             break;
         default:
-            $resource_from_name = "无";
+            $take_type_name = "无";
     }
-    return $resource_from_name;
+    return $take_type_name;
 }
 
 function getInColumnName($in_column){
