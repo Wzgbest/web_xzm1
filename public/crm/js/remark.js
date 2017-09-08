a97cac144bd3d843f47703835430c30af1cfc88a
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-09-08 07:25:32 +0000

    合并代码自wcc

---
+++ public/crm/js/remark.js
@@ -0,0 +1,21 @@
+
+$(document).on("click",".remark i.fa-pencil",function(){
+    $(this).siblings("input").removeAttr("readonly").focus().val("");
+    $(this).addClass("hide").siblings(".fa-check").removeClass("hide");
+});
+$(document).on("click",".remark i.fa-close",function(){
+    $(this).parent(".remark").remove();
+});
+$(document).on("click",".remark i.fa-check",function(){
+    $(this).siblings("input").attr("readonly","readonly");
+    $(this).addClass("hide").siblings(".fa-pencil").removeClass("hide");
+});
+$(document).on("click",".remark input",function(){
+    if($(this).attr("readonly")){
+        $(this).parent(".remark").siblings("textarea[name='remark']").append($(this).val());
+    }
+});
+var txt = '<span class="remark"><input type="text" placeholder="请输入" /><i class="fa fa-pencil hide"></i><i class="fa fa-check"></i><i class="fa fa-close"></i></span>';
+$(".u-addRemark").click(function(){
+    $(this).before(txt).siblings(".remark").last().children("input").focus();
+});
