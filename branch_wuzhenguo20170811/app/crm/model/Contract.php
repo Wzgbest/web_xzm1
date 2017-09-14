6985b139abf3b70cf48928577229e072c780e33d
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-09-14 01:23:48 +0000

    更新代码自主干

--- branch_wuzhenguo20170811/app/crm/model/Contract.php
+++ branch_wuzhenguo20170811/app/crm/model/Contract.php
@@ -662,4 +662,20 @@ class Contract extends Base{
         $map["status"] = 5;
         return $this->model->table($this->dbprefix."contract")->where($map)->update($data);
     }
+    //上次合同申请的申请人
+    public function getLastApply($con){
+        $subQuery=$this->model
+            ->table($this->table)
+            ->where($con)
+            ->field("contract_type,contract_apply_1,contract_apply_2,contract_apply_3,contract_apply_4,contract_apply_5,contract_apply_6")
+            ->order("update_time","DESC")
+            ->buildSql();
+        //var_exp($subQuery,'$subQuery',1);
+        $lastAppliedList = $this->model
+            ->table($subQuery." l")
+            ->group("contract_type")
+            ->select();
+        return $lastAppliedList;
+        //return $this->model->table($this->table)->where($con)->field("contract_type,contract_apply_1,contract_apply_2,contract_apply_3,contract_apply_4,contract_apply_5,contract_apply_6")->order("update_time","DESC")->group("contract_type")->select();
+    }
 }
