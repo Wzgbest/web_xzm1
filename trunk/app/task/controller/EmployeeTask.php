c7f6706c1df95e9bfbf57810084c24ecf8e8dcf3
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-09-21 01:11:08 +0000

    合并代码自wcc

--- trunk/app/task/controller/EmployeeTask.php
+++ trunk/app/task/controller/EmployeeTask.php
@@ -29,7 +29,7 @@ class EmployeeTask extends Initialize{
 		$uid = $user_info['userid'];
 		$employeeTaskModel = new EmployeeTaskModel($this->corp_id);
 		$task_list = $employeeTaskModel->getEmployeeTaskList($uid,$num,$last_id,$task_type);
-		
+		var_exp($task_list);
 		/*
 			不需要直接获取用户的评论，可以点击后在获取用户评论
 			暂时用不到
@@ -92,5 +92,49 @@ class EmployeeTask extends Initialize{
 		$result['data'] = $my_task_list;
 		return json($result);
 	}
+
+    /**
+     * 任务大厅里的任务
+     */
+	public function get_task_list(){
+        $result = ['status'=>0,'info'=>"获取列表时失败!"];
+
+        $num = input('num',10,'int');
+        $p = input("p",1,"int");
+        $task_type = input('task_type',0,'int');
+
+        $map=[];
+        if($task_type)
+        {
+            $map['task_type']=$task_type;
+        }
+        $user_info = get_userinfo();
+        $uid = $user_info['userid'];
+        $employeeTaskModel = new EmployeeTaskModel($this->corp_id);
+        $task_list = $employeeTaskModel->getEmployeeTaskList($uid,$num,$p,$field='*',$order="id",$direction="desc",$map);
+        $countField=["
+        count(1) as `0`,
+        sum((case when task_type = 1 then 1 else 0 end)) as `1`,
+        sum((case when task_type =2 then 1 else 0 end)) as `2`,
+        sum((case when task_type =3 then 1 else 0 end)) as `3`,
+        sum((case when task_type =4 then 1 else 0 end)) as `4`
+        "];//统计个数的field
+        $task_count=$employeeTaskModel->getEmployeeTaskCount($uid,$countField,$con=[]);
+        $this->assign('task_list',$task_list);
+        $this->assign('task_count',$task_count);
+    }
+    public function reward_task()
+    {
+        return view();
+    }
+    public function hot_task()
+    {
+        $this->get_task_list();
+        return view();
+    }
+    public function hot_task_load(){
+        $this->get_task_list();
+        return view();
+    }
 
 }
