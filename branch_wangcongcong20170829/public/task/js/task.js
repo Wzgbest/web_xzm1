a197b926459bb719d26dbd96379c81e36f21f1e0
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-09-23 09:40:25 +0000

    更新代码自主干

--- branch_wangcongcong20170829/public/task/js/task.js
+++ branch_wangcongcong20170829/public/task/js/task.js
@@ -366,11 +366,56 @@ function new_task_form(load_table){
         console.log(val);
 
         if(val==1){
-            loadPage('/task/index/new_task/fr/'+self.load_table,self.load_table);
+            $.ajax({
+                url: '/task/index/new_task/fr/'+self.load_table,
+                type: 'get',
+                success: function(data) {
+                    //console.log(data);
+                    //console.log($("#"+self.load_table+" .new_task_panel"));
+                    //console.log($("#"+self.load_table+" .new_task_panel .new_task_info_panel"));
+                    $("#"+self.load_table+" .new_task_panel .new_task_info_panel").html(data);
+                    $("#"+self.load_table+" .task_list").addClass("hide");
+                    $("#"+self.load_table+" .new_task_panel").removeClass("hide");
+                },
+                error: function() {
+                    layer.msg('加载任务新建出现错误',{icon:2});
+                }
+            });
+            //loadPage('/task/index/new_task/fr/'+self.load_table,self.load_table);
         }else if(val==2){
-            loadPage('/task/index/PKnew_task/fr/'+self.load_table,self.load_table);
+            $.ajax({
+                url: '/task/index/PKnew_task/fr/'+self.load_table,
+                type: 'get',
+                success: function(data) {
+                    //console.log(data);
+                    //console.log($("#"+self.load_table+" .new_task_panel"));
+                    //console.log($("#"+self.load_table+" .new_task_panel .new_task_info_panel"));
+                    $("#"+self.load_table+" .new_task_panel .new_task_info_panel").html(data);
+                    $("#"+self.load_table+" .task_list").addClass("hide");
+                    $("#"+self.load_table+" .new_task_panel").removeClass("hide");
+                },
+                error: function() {
+                    layer.msg('加载任务新建出现错误',{icon:2});
+                }
+            });
+            //loadPage('/task/index/PKnew_task/fr/'+self.load_table,self.load_table);
         }else if(val==3){
-            loadPage('/task/index/rewardnew_task/fr/'+self.load_table,self.load_table);
+            $.ajax({
+                url: '/task/index/rewardnew_task/fr/'+self.load_table,
+                type: 'get',
+                success: function(data) {
+                    //console.log(data);
+                    //console.log($("#"+self.load_table+" .new_task_panel"));
+                    //console.log($("#"+self.load_table+" .new_task_panel .new_task_info_panel"));
+                    $("#"+self.load_table+" .new_task_panel .new_task_info_panel").html(data);
+                    $("#"+self.load_table+" .task_list").addClass("hide");
+                    $("#"+self.load_table+" .new_task_panel").removeClass("hide");
+                },
+                error: function() {
+                    layer.msg('加载任务新建出现错误',{icon:2});
+                }
+            });
+            //loadPage('/task/index/rewardnew_task/fr/'+self.load_table,self.load_table);
         }
     });
 
@@ -386,7 +431,7 @@ function new_task_form(load_table){
             url: "/task/index/pay/money/"+money+"/fr/"+self.load_table,
             type: 'get',
             success: function(data) {
-                console.log(data);
+                //console.log(data);
                 console.log($("#"+self.load_table+" .pay_ui"));
                 $("#"+self.load_table+" .pay_ui").html(data);
                 $("#"+self.load_table+" .pay_ui .payPwd").payPwd({
@@ -407,6 +452,10 @@ function new_task_form(load_table){
     $("#"+self.load_table+" .pay_ui").on("click",".pop-submit-btn",function(){
         console.log("pop-submit-btn");
         self.add_task(self.get_pay_password());
+    });
+    $("#"+self.load_table+" .new_task_cancel").click(function(){
+        $("#"+self.load_table+" .task_info_panel").addClass("hide");
+        $("#"+self.load_table+" .task_list").removeClass("hide");
     });
 }
 
@@ -998,7 +1047,7 @@ function task_details(load_table,id,type){
             type: 'get',
             success: function(data) {
                 //console.log(data);
-                //console.log($(task_details_sel+" .tip_ui .mid"));
+                //console.log($(task_details_sel+" .tip_ui"));
                 $(task_details_sel+" .pay_ui").html(data);
                 $(task_details_sel+" .pay_ui .payPwd").payPwd({
                     max:6,
