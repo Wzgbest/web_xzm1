1e09c1166c530e4c491ac10cc9799e02500ac635
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-09-26 01:30:29 +0000

    合并代码自wcc

--- trunk/app/task/model/EmployeeTask.php
+++ trunk/app/task/model/EmployeeTask.php
@@ -229,7 +229,7 @@ class EmployeeTask extends Base{
             ->join($this->dbprefix.'employee_task_like etl',"etl.task_id = et.id and etl.user_id = '$uid'","LEFT")
             ->join($this->dbprefix.'red_envelope re',"re.task_id = et.id and re.took_user = ".$uid,"LEFT")
             ->join($this->dbprefix.'employee_task_guess tg',"tg.task_id=et.id and tg.guess_employee=".$uid,"LEFT")
-            ->field($field)->where($map_str)->where($map)->order($listOrder)->select();
+            ->field($field)->where($map_str)->where($map)->group('et.id')->order($listOrder)->select();
         return $employee_task_list;
 
     }
