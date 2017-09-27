d51a191ad6d47ae28e765fe06a024cd15ac5c60d
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-09-27 08:11:20 +0000

    合并代码自cxx

--- trunk/app/crm/model/SaleOrderContractItem.php
+++ trunk/app/crm/model/SaleOrderContractItem.php
@@ -26,6 +26,28 @@ class SaleOrderContractItem extends Base{
             ->select();
     }
 
+    public function getContractItemBySaleIdArr($sale_id_arr){
+        if(empty($sale_id_arr)){
+            return [];
+        }
+        $field = [
+            "soci.*",
+            "cs.contract_name as contract_type_name",
+            "co.contract_no",
+            "sob.id as bill_id",
+            "sob.status as bill_status",
+        ];
+        return $this->model->table($this->table)->alias('soci')
+            ->join($this->dbprefix.'sale_order_contract soc','soci.sale_order_id = soc.id',"LEFT")
+            ->join($this->dbprefix.'contract co','co.id = soci.contract_id',"LEFT")
+            ->join($this->dbprefix.'contract_applied ca','ca.id = co.applied_id',"LEFT")
+            ->join($this->dbprefix.'contract_setting cs','cs.id = ca.contract_type',"LEFT")
+            ->join($this->dbprefix.'sale_order_bill sob','sob.contract_id = soci.contract_id',"LEFT")
+            ->where('soc.sale_id',"in",$sale_id_arr)
+            ->field($field)
+            ->select();
+    }
+
     /**添加
      * @param $data array 销售单合同数据
      * @return false|\PDOStatement|int|\think\Collection
