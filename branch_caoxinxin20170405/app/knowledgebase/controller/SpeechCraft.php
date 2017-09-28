d897e0b8a7e22113094632a00208c5b0e759e220
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-09-28 01:48:10 +0000

    更新代码自主干

--- branch_caoxinxin20170405/app/knowledgebase/controller/SpeechCraft.php
+++ branch_caoxinxin20170405/app/knowledgebase/controller/SpeechCraft.php
@@ -35,10 +35,19 @@ class SpeechCraft extends Initialize{
     添加新文章
      */
     public function add_article(){
-        $article_type = $this->_speechCraftModel->getAllArticleType();
-
-        $this->assign('article_type',$article_type);
-        return view('new');
+        $id = input('id',0,'int');
+        // var_dump($id);die();
+        if ($id) {
+            $article_info = $this->_speechCraftModel->getOneArticleById($id);
+            $article_type = $this->_speechCraftModel->getAllArticleType();
+            $this->assign('article_type',$article_type);
+            $this->assign('article',$article_info);
+            return view('edit');
+        }else{
+            $article_type = $this->_speechCraftModel->getAllArticleType();
+            $this->assign('article_type',$article_type);
+            return view('new');
+        }
     }
 
     public function add_class_page(){
@@ -142,6 +151,98 @@ class SpeechCraft extends Initialize{
         return json($result);
 
     }
+
+ /*
+    编辑文章
+     */
+    public function editArticle(){
+        $result = ['status'=>0,'info'=>"跟新文章失败!"];
+
+        $userinfo = get_userinfo();
+        $id = input('article_id',0,'int');
+        $uid = $userinfo['userid'];
+        $data = [];
+        $data['article_name'] = input('article_name','',"string");
+        $data['article_type'] = input('article_type',1,"int");
+        $data['article_class'] = input('article_class',0,"int");
+        $data['article_content'] = input('article_content','',"string");
+        $data['article_url'] = input('article_url','',"string");
+        $data['article_text'] = input('article_text');
+        $data['article_is_top'] = input('article_is_top',0,"int");
+        $data['article_start_top_time'] = input('article_start_top_time','',"string");
+        $data['article_end_top_time'] = input('article_end_top_time','',"string");
+        $data['article_start_show_time'] = input('article_start_show_time','',"string");
+        $data['article_end_show_time'] = input('article_end_show_time','',"string");
+        $data['article_release_type'] = input('article_release_type',0,"int");
+        $data['article_release_time'] = input('article_release_time','',"string");
+        $data['article_creat_time'] = time();
+        $data['article_edit_time'] = time();
+        $data['add_user'] = $uid;
+        // var_dump(input('post.'));
+        // var_dump($data);die();
+        if (empty($data['article_name'])) {
+            $result['message'] = "文章名不能为空!";
+            return json($result);
+        }
+        if (empty($data['article_class'])) {
+            $result['message'] = "文章分类不能为空!";
+            return json($result);
+        }
+        if (empty($data['article_content'])) {
+            $result['message'] = "文章简介不能为空!";
+            return json($result);
+        }
+        if ($data['article_type'] == 1) {
+            if (empty($data['article_text'])) {
+                $result['message'] = "文章正文不能为空!";
+                return json($result);
+            }
+        }else{
+            if (empty($data['article_url'])) {
+                $result['message'] = "文章链接不能为空!";
+                return json($result);
+            }
+        }
+        if ($data['article_is_top']) {
+            if (empty($data['article_start_top_time']) || empty($data['article_end_top_time'])) {
+                $result['message'] = "请选择置顶时间!";
+                return $result;
+            }
+        }
+        if ($data['article_release_type']) {
+            if (empty($data['article_release_time'])) {
+                $result['message'] = "请选择发布时间!";
+                return $result;
+            }
+        }
+
+        if (!empty($data['article_start_top_time'])) {
+            $data['article_start_top_time'] = strtotime(str_replace('T',' ',$data['article_start_top_time']));
+        }
+        if (!empty($data['article_end_top_time'])) {
+            $data['article_end_top_time'] = strtotime(str_replace('T',' ',$data['article_end_top_time']));
+        }
+        if (!empty($data['article_start_show_time'])) {
+            $data['article_start_show_time'] = strtotime(str_replace('T',' ',$data['article_start_show_time']));
+        }
+        if (!empty($data['article_end_show_time'])) {
+            $data['article_end_show_time'] = strtotime(str_replace('T',' ',$data['article_end_show_time']));
+        }
+        if (!empty($data['article_release_time'])) {
+            $data['article_release_time'] = strtotime(str_replace('T',' ',$data['article_release_time']));
+        }
+        // var_dump($id);die();
+        $id = $this->_speechCraftModel->editArticleInfo($id,$data);
+        // var_dump($id);die();
+        if ($id) {
+            $result['status'] = 1;
+            $result['info'] = "跟新文章成功";
+        }
+        // var_dump($id);die();
+        return json($result);
+
+    }
+
     /**
      * 添加分类
      */
@@ -183,6 +284,106 @@ class SpeechCraft extends Initialize{
     public function show(){
         $this->_showArticleInfo();
         return view('detail');
+    }
+
+    /**
+     * 删除文章
+     */
+    public function delete(){
+        $result = ['status'=>0,'info'=>"删除失败"];
+        $article_id = input('article_id',0,'int');
+        if (!$article_id) {
+            $result['info'] = "文章编号为空";
+            return $result;
+        }
+        $flg = $this->_speechCraftModel->delOneArticleById($article_id);
+        if ($flg > 0) {
+            $result['info'] = "删除成功";
+            $result['status'] = 1;
+        }
+        return json($result);
+
+    }
+
+    /**
+     * 删除分类
+     * @return [type] [description]
+     */
+    public function deleteClass(){
+        $result = ['status'=>0,'info'=>"删除分类失败"];
+        $class_id = input('class_id',0,'int');
+        if (!$class_id) {
+            $result['info'] = "分类id为空";
+            return json($result);
+        }
+        $article_info = $this->_speechCraftModel->getAllArticle('',$class_id);
+        if (!empty($article_info)) {
+            $result['info'] = "该分类下还有文章,不能删除分类";
+            return json($result);
+        }
+        $flg = $this->_speechCraftModel->deleteClassById($class_id);
+        if ($flg>0) {
+            $result['info'] = "删除分类成功";
+            $result['status'] = 1;
+        }
+        return json($result);
+    }
+
+    /**
+     * 修改文章分类
+     */
+    public function changeClass(){
+        $result = ['status'=>0,'info'=>"修改失败"];
+        $article_id = input('article_id',0,'int');
+        $class_id = input('class_id',0,'int');
+        if (!$article_id || !$class_id) {
+            $result['info'] = "参数错误";
+            return json($result);
+        }
+        $data['article_class'] = $class_id;
+        $flg = $this->_speechCraftModel->editArticleInfo($article_id,$data);
+        if ($flg > 0) {
+            $result['info'] = "修改成功";
+            $result['status'] = 1;
+        }
+        return json($result);
+    }
+
+    /**
+     * 修改文章置顶
+     */
+    public function changeClass(){
+        $result = ['status'=>0,'info'=>"修改失败"];
+        $article_id = input('article_id',0,'int');
+        $is_top = input('is_top',0,'int');
+        $article_start_top_time = input('article_start_top_time','',"string");
+        $article_end_top_time = input('article_end_top_time','',"string");
+        if (!$article_id || !$is_top || !$article_start_top_time || !$article_end_top_time) {
+            $result['info'] = "参数错误";
+            return json($result);
+        }
+        $data['is_top'] = $is_top;
+        $data['article_start_top_time'] = $article_start_top_time;
+        $data['article_end_top_time'] = $article_end_top_time;
+        $flg = $this->_speechCraftModel->editArticleInfo($article_id,$data);
+        if ($flg > 0) {
+            $result['info'] = "修改成功";
+            $result['status'] = 1;
+        }
+        return json($result);
+    }
+
+
+    public function deleteSpeechClass(){
+        return view('delete_speech_class');
+    }
+
+    public function deleteSpeechList(){
+        return view('delete_speech_list');
+    }
+
+    public function changeSpeechClass(){
+        return view('change_class');
     }
 
     /**
