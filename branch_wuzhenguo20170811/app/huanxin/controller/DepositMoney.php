9a691d1d44a954f20fcf02fb5cf01f352ad0bbb6
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-10-16 08:18:54 +0000

    更新代码自主干

--- branch_wuzhenguo20170811/app/huanxin/controller/DepositMoney.php
+++ branch_wuzhenguo20170811/app/huanxin/controller/DepositMoney.php
@@ -6,16 +6,15 @@
 namespace app\huanxin\controller;
 
 use app\common\model\Corporation;
-use app\huanxin\controller\User;
 use app\huanxin\model\TakeCash;
 use app\huanxin\service\DepositMoney as DepositMoneyService;
 use app\huanxin\model\AppAlipayTrade;
 use app\common\model\Employee;
+use app\common\controller\Initialize;
 
-class DepositMoney
-{
-    public function index()
-    {
+class DepositMoney extends Initialize{
+    public function _initialize(){
+        parent::_initialize();
     }
 
     /**
@@ -23,15 +22,10 @@ class DepositMoney
      * @param userid
      * @param access_token
      * @param money 充值金额，单位元，3.33
-     * @param \app\huanxin\controller\User $user
      * @return string
      */
-    public function createAppPrepay(User $user)
-    {
-        $userid = input('param.userid');
-        $access_token = input('param.access_token');
+    public function createAppPrepay(){
         $money = input('param.money');
-
         $info['status'] = false;
         if (!preg_match('/^[0-9]{1,30}\.[0-9]{1,2}$/',$money)) {
             $info['message'] = '用户充值金额格式不正确';
@@ -44,15 +38,11 @@ class DepositMoney
             $info['errnum'] = 2;
             return json($info);
         }
-        $r = $user->checkUserAccess($userid,$access_token);
-        if (!$r['status']) {
-            return json($r);
-        }
 
         $out_trade_no = 'guguo_app_pay'.date('YmdHis',time()).time().rand(1000,9999);
         $data = [
-            'corp_id' => $r['userinfo']['corpid'],
-            'userid' =>$r['userinfo']['id'],
+            'corp_id' => $this->corp_id,
+            'userid' =>$this->uid,
             'money' =>$total_money,
             'out_trade_no' =>$out_trade_no,
             'create_time' =>time(),
@@ -65,7 +55,7 @@ class DepositMoney
             $info['errnum'] = 0;
             $info['out_trade_no'] = $out_trade_no;
             $info['paymoney'] = $money;
-            write_log($r['userinfo']['id'],5,'生成用户订单，订单号'.$out_trade_no,$r['corp_id']);
+            write_log($this->uid,5,'生成用户订单，订单号'.$out_trade_no,$this->corp_id);
         } else {
             $info['message'] = '生成订单失败';
             $info['errnum'] = 3;
@@ -80,13 +70,11 @@ class DepositMoney
      * @param money 充值金额，单位元，3.33
      * @param trade_no 支付宝系统内部账单
      * @param out_trade_no 生成的订单号返回给app端
-     * @param \app\huanxin\controller\User $user
+     
      * @return string
      */
-    public function getAppNotice(User $user)
-    {
-        $userid = input('param.userid');
-        $access_token = input('param.access_token');
+    public function getAppNotice(){
+        $user_info = get_userinfo();
         $money = input('param.money');
         $trade_no = input('param.trade_no');
         $out_trade_no = input('param.out_trade_no');
@@ -108,10 +96,6 @@ class DepositMoney
             $info['errnum'] = 3;
             return json($info);
         }
-        $r = $user->checkUserAccess($userid,$access_token);
-        if (!$r['status']) {
-            return json($r);
-        }
         $trade_info = AppAlipayTrade::getTradeInfo($out_trade_no);
         if (empty($trade_info)) {
             $info['message'] = '提交的订单不存在';
@@ -121,10 +105,10 @@ class DepositMoney
         if ($trade_info['status'] ==1) {
             $info['message'] = '该订单已在系统中充值，不要重复提交';
             $info['errnum'] = 5;
-            write_log($r['userinfo']['id'],0,'app充值刷单嫌疑',$r['corp_id']);
+            write_log($this->uid,0,'app充值刷单嫌疑',$this->corp_id);
             return json($info);
         }
-        $depoM = new DepositMoneyService($r['corp_id']);
+        $depoM = new DepositMoneyService($this->corp_id);
         $res = $depoM->queryTradeNumber($trade_no,$out_trade_no,$money);
         if (!$res['status']) {
             $res['errnum'] = 6;
@@ -132,11 +116,11 @@ class DepositMoney
         }
 
         //兑换货币
-        $left_money = $total_money + $r['userinfo']['left_money'];
-        $cashM = new TakeCash($r['corp_id']);
+        $left_money = $total_money + $user_info['userinfo']['left_money'];
+        $cashM = new TakeCash($this->corp_id);
         //take_cash记录
         $cash_data = [
-            'userid'=>$r['userinfo']['id'],
+            'userid'=>$this->uid,
             'take_money'=> $total_money,
             'status'=>2,
             'took_time'=>time(),
@@ -150,7 +134,8 @@ class DepositMoney
         ];
         $cashM->link->startTrans();
         try {
-            $add = $user->employM->setEmployeeSingleInfo($userid,['left_money' => $left_money]);
+            $employM = new Employee($this->corp_id);
+            $add = $employM->setEmployeeSingleInfo($this->telephone,['left_money' => $left_money]);
             $cash_rec = $cashM->addOrderNumber($cash_data);
             $app_r = AppAlipayTrade::setTradeStatus($out_trade_no,$app_data);
         } catch (\Exception $e){
@@ -158,13 +143,16 @@ class DepositMoney
         }
         if ($add > 0 && $cash_rec > 0 && $app_r > 0) {
             $cashM->link->commit();
-            write_log($r['userinfo']['id'],5,'用户充值成功,总金额'.$total_money.'分',$r['corp_id']);
+            $employM = new Employee($this->corp_id);
+            $user_info = $employM->getEmployeeByTel($this->telephone);
+            set_userinfo($this->corp_id,$this->telephone,$user_info);
+            write_log($this->uid,5,'用户充值成功,总金额'.$total_money.'分',$this->corp_id);
             $info['status'] = true;
             $info['message'] = '用户充值成功';
             $info['errnum'] = 0;
         } else {
             $cashM->link->rollback();
-            write_log($r['userinfo']['id'],5,'用户充值成功,兑换系统货币失败，总金额'.$total_money.'分',$r['corp_id']);
+            write_log($this->uid,5,'用户充值成功,兑换系统货币失败，总金额'.$total_money.'分',$this->corp_id);
             send_mail(config('system_email.user'),config('system_email.pass'),'wangqiwen@winbywin.com','充值问题',config('system_email.from_name'),'支付宝充值成功，兑换货币失败'.json_encode($cash_data,true));
             $info['message'] = '兑换系统货币失败，联系管理员';
             $info['errnum'] = 7;
@@ -229,6 +217,9 @@ class DepositMoney
             if ($add > 0 && $cash_rec > 0) {
                 $employM->link->commit();
                 Corporation::commit();
+                $employM = new Employee($this->corp_id);
+                $user_info = $employM->getEmployeeByTel($this->telephone);
+                set_userinfo($this->corp_id,$this->telephone,$user_info);
                 write_log($alipay_info['userid'],5,'用户充值成功,总金额'.$in_money.'分',$corp_id['corp_id']);
                 return 'success';
             } else {
