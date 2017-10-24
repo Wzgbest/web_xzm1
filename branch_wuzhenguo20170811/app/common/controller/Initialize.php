da6f84eb3e77aca92251e6c64483cd1d51bf11db
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-10-24 09:03:37 +0000

    更新代码自主干

--- branch_wuzhenguo20170811/app/common/controller/Initialize.php
+++ branch_wuzhenguo20170811/app/common/controller/Initialize.php
@@ -18,21 +18,32 @@ class Initialize extends Controller
     protected $device_type;
 
     public function _initialize(){
-        $this->telephone = input('userid','',"string");
-        $this->access_token = input('access_token','',"string");
-        if (!($this->telephone && $this->access_token)) {
+        $telephone = input('userid','',"string");
+        $access_token = input('access_token','',"string");
+        $cookie_access_token = get_token_by_cookie();
+        if (is_web($telephone,$access_token,$cookie_access_token)){
             $this->device_type = 1;
-            $this->access_token = get_token_by_cookie();
+            $this->access_token = $cookie_access_token;
             //var_exp($this->access_token,'$this->access_token',1);
             if(!$this->access_token){
                 $this->redirectToLogin();
             }
             $this->telephone = get_telephone_by_token($this->access_token);
+        }else{
+            if (!($telephone&&$access_token)){
+                $info['message'] = '用户id为空或token为空';
+                $info['errnum'] = 101;
+                $this->return_error($info);
+            }
+            $this->telephone = $telephone;
+            $this->access_token = $access_token;
         }
         //var_exp($this->telephone,'$this->telephone');
         //var_exp($this->access_token,'$this->access_token',1);
         $info = check_telephone_and_token($this->telephone,$this->access_token);
-        $this->device_type = $info["device_type"];
+        if(!$this->device_type){
+            $this->device_type = $info["device_type"];
+        }
         if($info["status"]==false) {
             if($this->device_type==1){
                 $this->redirectToLogin();
