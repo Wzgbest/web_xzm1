f8388c791e51c6f1a97d32eb900af3558858cd7a
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-10-27 04:44:20 +0000

    合并代码自cxx

--- trunk/app/index/controller/Index.php
+++ trunk/app/index/controller/Index.php
@@ -31,17 +31,26 @@ class Index extends Initialize{
     public function select_window(){
         $structureEmployeeModel = new StructureEmployee($this->corp_id);
         $structures = $structureEmployeeModel->getAllStructureAndEmployee();
+        $structure_employee = [];
+        $structure_pid = [];
         foreach ($structures as &$structure){
             $structure["employee_ids_arr"] = explode(",",$structure["employee_ids"]);
+            $structure_employee[$structure["id"]] = explode(",",$structure["employee_ids"]);
+            $structure_pid[$structure["id"]] = $structure["struct_pid"];
         }
         $employM = new Employee($this->corp_id);
         $friendsInfos = $employM->getAllUsers();
         $employees = [];
+        $employee_name = [];
         foreach ($friendsInfos as $friendsInfo){
             $employees[$friendsInfo["id"]] = $friendsInfo;
+            $employee_name[$friendsInfo["id"]] = $friendsInfo["nickname"];
         }
         $this->assign("structures",$structures);
         $this->assign("employees",$employees);
+        $this->assign("structure_employee",json_encode($structure_employee,true));
+        $this->assign("structure_pid",json_encode($structure_pid,true));
+        $this->assign("employee_name",json_encode($employee_name,true));
         return view();
     }
 
