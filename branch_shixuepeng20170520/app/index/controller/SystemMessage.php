df10c7e2e64988e00129ec6b0eb66eaef60f3244
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-10-31 03:50:12 +0000

    更新代码自主干

---
+++ branch_shixuepeng20170520/app/index/controller/SystemMessage.php
@@ -0,0 +1,45 @@
+<?php
+// +----------------------------------------------------------------------
+// | 中迅传媒 [ 纯粹、极致到零 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017 http://www.baidusd.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: blu10ph <blu10ph@gmail.com> <http://www.blu10ph.cn>
+// +----------------------------------------------------------------------
+namespace app\index\controller;
+
+use app\common\controller\Initialize;
+use think\Db;
+use think\Controller;
+use app\common\model\Employee;
+use app\common\model\StructureEmployee;
+
+class SystemMessage extends Initialize{
+    public function _initialize(){
+        parent::_initialize();
+    }
+    public function get_msg_list(){
+
+    }
+    public function get_msg(){
+
+    }
+    public function add_msg(){
+
+    }
+    public function del_msg(){
+
+    }
+    public function push_msg(){
+
+    }
+    public function set_read_msg(){
+
+    }
+    public function set_read_msg_by_type(){
+
+    }
+    public function set_read_msg_all(){
+
+    }
+}
