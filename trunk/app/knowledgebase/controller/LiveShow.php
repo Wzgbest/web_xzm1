7cfc677370dcceb8b0a75e5caf646e0b1a6f7b14
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-11-01 07:04:13 +0000

    合并代码自wzg

--- trunk/app/knowledgebase/controller/LiveShow.php
+++ trunk/app/knowledgebase/controller/LiveShow.php
@@ -1,7 +1,27 @@
 <?php
 namespace app\knowledgebase\controller;
 
-class LiveShow
+use app\common\controller\Initialize;
+use app\knowledgebase\service\VhallApi as VhallApi;
+class LiveShow extends Initialize
 {
+	public function index(){
+		$vhallApi = new VhallApi();
 
+		$act_list = $vhallApi->getActivityList();
+		
+		foreach ($act_list['data']['lists'] as &$value) {
+			$value['url'] = "http://e.vhall.com/webinar/inituser/".$value['webinar_id'];
+		}
+		// var_dump($act_list['data']['lists']);die();
+		$this->assign('list',$act_list['data']);
+		return view();
+	}
+
+	public function test(){
+		$vhallApi = new VhallApi();
+
+		$info = $vhallApi->startActivity("484061604");
+		return $info;
+	}
 }
