67e00cf3a540336375e0788a02a8525645631f18
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-11-01 07:05:03 +0000

    更新代码自主干

--- branch_caoxinxin20170405/public/verification/js/verification_contract.js
+++ branch_caoxinxin20170405/public/verification/js/verification_contract.js
@@ -6,56 +6,9 @@
 // | Author: blu10ph <blu10ph@gmail.com> <http://www.blu10ph.cn>
 // +----------------------------------------------------------------------
 
-$("#frames #verification-contractfr .verification_contract .u-tabList .u-tabOperation .approved").click(function(){
-    var id = $(this).parent().siblings().children("input").val();
-    var remark = "";
-    if($(this).hasClass("remark")){
-        remark = prompt("请输入备注","");
-        if (remark==null || remark==""){
-            return;
-        }
-    }
-    var data = "id="+id+"&remark="+remark;
-    $.ajax({
-        url: '/verification/contract/approved',
-        type: 'post',
-        data: data,
-        success: function(data) {
-            //console.log(data);
-            layer.msg(data.info,{icon:data.status==1?1:2});
-            if(data.status) {
-                verification_contract_list_manage.reload_list();
-            }
-        },
-        error: function() {
-            layer.msg('通过合同申请时发生错误!',{icon:2});
-        }
-    });
-});
 
-$("#frames #verification-contractfr .verification_contract .u-tabList .u-tabOperation .rejected").click(function(){
-    var id = $(this).parent().siblings().children("input").val();
-    var remark = "";
-    if($(this).hasClass("remark")){
-        remark = prompt("请输入备注","");
-    }
-    var data = "id="+id+"&remark="+remark;
-    $.ajax({
-        url: '/verification/contract/rejected',
-        type: 'post',
-        data: data,
-        success: function(data) {
-            //console.log(data);
-            layer.msg(data.info,{icon:data.status==1?1:2});
-            if(data.status) {
-                verification_contract_list_manage.reload_list();
-            }
-        },
-        error: function() {
-            layer.msg('驳回合同申请时发生错误!',{icon:2});
-        }
-    });
-});
+
+
 
 $("#frames #verification-contractfr .verification_contract .u-tabList .u-tabOperation .invalid").click(function(){
     var id = $(this).parent().siblings().children("input").val();
