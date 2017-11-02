7363abbec3fcd0485faf26bf438b8c6148400fd9
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-11-02 03:03:48 +0000

    合并代码自cxx

--- public/crm/js/sale_chance_index.js
+++ public/crm/js/sale_chance_index.js
@@ -6,3 +6,8 @@
 // | Author: blu10ph <blu10ph@gmail.com> <http://www.blu10ph.cn>
 // +----------------------------------------------------------------------
 
+$(".crm_sale_chance .myAllSaleChancePage .m-tableBox .customer_name").click(function(){
+    var id = $(this).attr("customer_id");
+    //console.log("id",id);
+    my_sale_chance_info_manage.general(id);
+});
