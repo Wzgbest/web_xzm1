c103f7841b533b7d8817f4e5fd2bb344b4a43a51
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-11-02 05:47:35 +0000

    更新代码自主干

--- branch_zhaojingtong20170405/public/crm/js/sale_chance_index.js
+++ branch_zhaojingtong20170405/public/crm/js/sale_chance_index.js
@@ -11,3 +11,9 @@ $(".crm_sale_chance .myAllSaleChancePage .m-tableBox .customer_name").click(func
     //console.log("id",id);
     my_sale_chance_info_manage.general(id);
 });
+$(".crm_sale_chance .myAllSaleChancePage .m-tableBox .sale_chance_show").click(function(){
+    var customer_id = $(this).attr("customer_id");
+    var sale_chance_id = $(this).attr("sale_chance_id");
+    //console.log("id",id);
+    my_sale_chance_info_manage.sale_chance_show(customer_id,sale_chance_id);
+});
