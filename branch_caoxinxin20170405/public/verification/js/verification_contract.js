1a2817997dcfc0b02660ade20d152be1fae17049
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-11-08 01:29:41 +0000

    更新代码自主干

--- branch_caoxinxin20170405/public/verification/js/verification_contract.js
+++ branch_caoxinxin20170405/public/verification/js/verification_contract.js
@@ -6,9 +6,53 @@
 // | Author: blu10ph <blu10ph@gmail.com> <http://www.blu10ph.cn>
 // +----------------------------------------------------------------------
 
+//通过
+$(".verification_contract_index_pop").on("click",".approved-page-pop .pop-submit-btn",function(){
+    let id = $(".verification_contract_index_pop").attr("id");
+    let remark = $(".verification_contract_index_pop .approved-page-pop .u-mark").val();
+    let data = "id="+id+"&remark="+remark;
+    $.ajax({
+        url: '/verification/contract/approved',
+        type: 'post',
+        data: data,
+        success: function(data) {
+            //console.log(data);
+            layer.msg(data.info,{icon:data.status==1?1:2});
+            if(data.status) {
+                verification_contract_list_manage.reload_list();
+            }
+        },
+        error: function() {
+            layer.msg('通过合同申请时发生错误!',{icon:2});
+        }
+    });
+}); 
 
 
-
+//驳回
+$(".verification_contract_index_pop").on("click",".rejected-page-pop .pop-submit-btn",function(){
+    let id = $(".verification_contract_index_pop").attr("id");
+    let remark = $(".contract-reject-radio:checked").val();
+    if(remark=="其他原因"){
+        remark=$(".verification_contract_index_pop .rejected-page-pop .u-mark").val();
+    }
+    let data = "id="+id+"&remark="+remark;
+    $.ajax({
+        url: '/verification/contract/rejected',
+        type: 'post',
+        data: data,
+        success: function(data) {
+            //console.log(data);
+            layer.msg(data.info,{icon:data.status==1?1:2});
+            if(data.status) {
+                verification_contract_list_manage.reload_list();
+            }
+        },
+        error: function() {
+            layer.msg('驳回合同申请时发生错误!',{icon:2});
+        }
+    });
+});
 
 $("#frames #verification-contractfr .verification_contract .u-tabList .u-tabOperation .invalid").click(function(){
     var id = $(this).parent().siblings().children("input").val();
