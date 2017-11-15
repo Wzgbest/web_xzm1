2cdb1587e466c9361460d347888cc909ccc48c1b
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-11-15 02:07:38 +0000

    更新代码自主干

--- branch_zhaojingtong20170405/public/static/call_js/fs_state.js
+++ branch_zhaojingtong20170405/public/static/call_js/fs_state.js
@@ -29,18 +29,20 @@ fs_state = function(){
 
 	  var operationUI = {
 	  		idle : function(ui){
+	  			console.log("电话空闲中");
 	  			ui.setTip("<b style='color:#275A33'>电话空闲中</b>");
-				ui.pos.CallBt.className = "calloutbt bkg";
-				ui.pos.HangBt.className = "hangupdsbt bkg";
+				ui.pos.CallBt.className = "bkg on";
+				ui.pos.HangBt.className = "hangupdsbt bkg off hide";
 				ui.pos.CallBt.title = "点击拨打";
 				ui.noCallMode();
 				ui.setDevolveStatus(0);
 				ui.setAskStatus(0);
 				ui.setKeepStatus(0);
 				ui.setMultiStatus(0);
-				ui.clearPhoneArea();// 去掉归属地显示
+				// ui.clearPhoneArea();// 去掉归属地显示
 	  		},
 	  		busy : function(ui){
+	  			console.log("通话中");
 				ui.setTip("<b style='color:#275A33'>通话中</b>");
 				ui.pos.CallBt.className = "calloutbt bkg";
 				ui.pos.HangBt.className = "hangupdsbt bkg";
@@ -50,9 +52,10 @@ fs_state = function(){
 				ui.setAskStatus(1);
 				ui.setKeepStatus(1);
 				ui.setMultiStatus(1);
-				ui.clearPhoneArea();// 去掉归属地显示
+				// ui.clearPhoneArea();// 去掉归属地显示
 	  		},
 	  		refuse : function(ui){
+	  			console.log("电话已拒接,可点击外呼");
 	  		  ui.setTip("<b style='color:#275A33'>电话已拒接,可点击外呼</b>");
               ui.pos.CallBt.className = "calloutbt bkg";
               ui.pos.HangBt.className = "hangupdsbt bkg";
@@ -64,6 +67,7 @@ fs_state = function(){
               ui.setMultiStatus(0);
             },
 	  		offline : function(ui){
+	  			console.log("电话已离线,不可接打电话");
 	  			ui.setTip("<b>电话已离线,不可接打电话</b>");
 				ui.pos.CallBt.className = "calldsbt bkg";
 				ui.pos.HangBt.className = "hangupdsbt bkg";
@@ -73,20 +77,31 @@ fs_state = function(){
 				ui.setAskStatus(0);
 				ui.setKeepStatus(0);
 				ui.setMultiStatus(0);
-				ui.clearPhoneArea();// 去掉归属地显示
+				// ui.clearPhoneArea();// 去掉归属地显示
 	  		},
 	  		agent_create : function(ui){
+	  			console.log(ui,"agent_create");
 	  			ui.pos.CallBt.className = ui.phone.phoneType == 3?"answerbt bkg":"calldsbt bkg";
+	  			console.log("A");
 				ui.pos.HangBt.className = "hangupbt bkg";
+				console.log("A");
 				ui.pos.CallBt.title = ui.phone.phoneType == 3?"点击接听":"请拿起话机接听,当前电话类型不支持点击接听";
+				console.log("A");
 				ui.callingMode();
+				console.log("A");
 				ui.setDevolveStatus(0);
+				console.log("A");
 				ui.setAskStatus(0);
+				console.log("A");
 				ui.setKeepStatus(0);
+				console.log("A");
 				ui.setMultiStatus(0);
+				console.log("A");
 				ui.setTip("(&nbsp;<b>请接听</b>)"),ui.ringMode();
+				console.log("A");
 	  		},
 	  		agent_ring : function(ui){
+	  			console.log("agent_ring");
 	  			ui.setTip("(&nbsp;<b>请接听</b>)");
 				ui.pos.CallBt.className = ui.phone.phoneType == 3?"answerbt bkg":"calldsbt bkg";
 				ui.pos.HangBt.className = "hangupbt bkg";
@@ -99,6 +114,7 @@ fs_state = function(){
 				ui.ringMode();
 	  		},
 	  		agent_answer : function(ui){
+	  			console.log("agent_answer");
 	  			ui.pos.CallBt.className = "answerdsbt bkg";
 				ui.pos.HangBt.className = "hangupbt bkg";
 				ui.pos.CallBt.title = "通话中,此按钮无效";
@@ -110,9 +126,11 @@ fs_state = function(){
                 ui.ringMode();
 	  		},
 	  		agent_hangup : function(ui){
+	  			console.log("agent_hangup");
 	  			ui.hangMode();
 	  		},
 	  		caller_create :function(ui){
+	  			console.log("caller_create");
 	  			if(ui.phone.callType == "caller_trunk"||(ui.phone.callType=="fs_agent"&&ui.phone.preState == "agent_create"))return; // 点击外呼(直线上线)特殊处理
 	  			ui.setTip("(&nbsp;<b>等待对方接听</b>)");
 				ui.pos.CallBt.className = "calldsbt bkg";
@@ -126,6 +144,7 @@ fs_state = function(){
 				ui.ringMode();
 	  		},
 	  		caller_answer : function(ui){
+	  			console.log("caller_answer");
 				ui.setTip("(&nbsp;<b>通话中</b>)");
 				ui.pos.CallBt.className = "calldsbt bkg";
 				ui.pos.HangBt.className = "hangupbt bkg";
@@ -138,12 +157,14 @@ fs_state = function(){
 				ui.ringMode();
 	  		},
 	  		caller_hangup : function(ui){
+	  			console.log("caller_hangup");
 	  			// ui.hangMode();
 				// this.clearPhoneArea();//去掉归属地显示
 				// ui.phone.callState = ui.phone.initState;//||"idle";
 				// -----ui.setStatus();
 	  		},
 	  		caller_ring : function(ui){
+	  			console.log("caller_ring");
 	  			ui.setTip("(&nbsp;<b>等待对方接听</b>)");
 				ui.pos.CallBt.className = "calldsbt bkg";
 				ui.pos.HangBt.className = "hangupbt bkg";
@@ -236,48 +257,56 @@ fs_state = function(){
 			   ],
 		callbacks: {
 				onagent_create : function(event, from, to,	args) {
+					console.log("客户呼入/点击外呼,座席响铃");
 					var phone =  phoneMsg(JSON.parse(args));
 					if(!phone)
 						return;
 					operationUI.agent_create(phone.callUI);
 				},
 				onagent_ring : function(event, from, to,args) {
+					console.log("座席响铃");
 					var phone =  phoneMsg(JSON.parse(args));
 					if(!phone)
 						return;
 					operationUI.agent_ring(phone.callUI);
 				},
 				onagent_answer : function(event, from, to,args) {
+					console.log("座席应答");
 					var phone =  phoneMsg(JSON.parse(args));
 					if(!phone)
 						return;
 					operationUI.agent_answer(phone.callUI);
 				},
 				onagent_hangup : function(event, from, to,args) {
+					console.log("座席挂机");
 					var phone =  phoneMsg(JSON.parse(args));
 					if(!phone)
 						return;
 					operationUI.agent_hangup(phone.callUI);
 				},
 				oncaller_create : function(event, from, to,args) {
+					console.log("座席呼出,客户响铃");
 					var phone =  phoneMsg(JSON.parse(args));
 					if(!phone)
 						return;
 					operationUI.caller_create(phone.callUI);
 				},
 				oncaller_ring : function(event, from, to,args) {
+					console.log("客户响铃");
 					var phone =  phoneMsg(JSON.parse(args));
 					if(!phone)
 						return;
 					operationUI.caller_ring(phone.callUI);
 				},
 				oncaller_answer : function(event, from, to,args) {
+					console.log("客户应答");
 					var phone =  phoneMsg(JSON.parse(args));
 					if(!phone)
 						return;
 					operationUI.caller_answer(phone.callUI);
 				},
 			 	oncaller_hangup : function(event, from, to,args) {
+			 		console.log("客户挂机");
 			 		var phone =  phoneMsg(JSON.parse(args));
 			 		if(!phone)
 						return;
@@ -287,18 +316,22 @@ fs_state = function(){
 
 			 	},          
 				onhand_idle : function(event, from, to, args) {
+					console.log("空闲中");
 				    var mess = JSON.parse(args);
 				    var phone =  phoneMsg(mess);
 				    if(!phone)
                         return;
 				    if(phone["currentState"]&&phone["currentState"]==mess.status){
+                        console.log("空闲中1");
                         return;
 				    }else{
                         phone["currentState"] = mess.status;
+                        console.log("空闲中2");
 				    }
                     operationUI.idle(phone.callUI);
 				},
 				onhand_busy : function(event, from, to, args) {
+					console.log("坐席繁忙");
 				    var mess = JSON.parse(args);
 					var phone =  phoneMsg(mess);    
 					if(!phone)
@@ -311,6 +344,7 @@ fs_state = function(){
 					operationUI.busy(phone.callUI);
 				},
 				onhand_offline : function(event, from, to, args) {
+					console.log("座席离线");
 				    var phone =  phoneMsg(JSON.parse(args));    
                     if(!phone)
                         return;
@@ -318,6 +352,7 @@ fs_state = function(){
                     operationUI.offline(phone.callUI);
 				},
 				onhand_refuse : function(event, from, to, args) {
+					console.log("座席拒接");
 				    
                     var phone =  phoneMsg(JSON.parse(args));    
                     if(!phone)
