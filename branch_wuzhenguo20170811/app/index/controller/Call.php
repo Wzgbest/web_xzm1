8e4e186325e1053daa541359151693969006b639
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-11-20 06:26:32 +0000

    更新代码自主干

---
+++ branch_wuzhenguo20170811/app/index/controller/Call.php
@@ -0,0 +1,83 @@
+<?php
+// +----------------------------------------------------------------------
+// | 中迅传媒 [ 纯粹、极致到零 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017 http://www.baidusd.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: blu10ph <blu10ph@gmail.com> <http://www.blu10ph.cn>
+// +----------------------------------------------------------------------
+namespace app\index\controller;
+
+use app\common\controller\Initialize;
+use think\Db;
+use think\Controller;
+use app\common\model\Employee;
+use app\common\model\StructureEmployee;
+use app\common\model\Meme;
+use app\common\model\RoleRule;
+use app\index\service\TQCallApi;
+use app\index\model\SystemMessage as SystemMessageModel;
+
+class Call extends Initialize{
+    public function _initialize(){
+        parent::_initialize();
+    }
+    public function call(){
+        $call_config = get_cache_by_tel($this->telephone,"call_config");
+        if(!$call_config){
+            $tq_config = config('tq');
+            $call_config["appid"] = $tq_config["appid"];
+            $call_config["appkey"] = $tq_config["appkey"];
+            $call_config["secretkey"] = strtoupper(md5($tq_config["appid"]."*(**)*".$tq_config["appkey"]));
+            $call_config["admin_uin"] = "9796221";
+            $call_config["uin"] = "9796249";
+            $call_config["strid"] = "sdzhcs2";
+            $call_config["time"] = time();
+            $tqCallApi = new TQCallApi();
+            $access_token_data = $tqCallApi->get_access_token($call_config["admin_uin"],$call_config["uin"],$call_config["strid"],$call_config["time"]);
+//            var_exp($access_token_data,'$access_token_data',1);
+            if(isset($access_token_data["errcode"])&&$access_token_data["errcode"]==0){
+                $call_config["access_token"] = $access_token_data["access_token"];
+                set_cache_by_tel($this->telephone,"call_config",$call_config,$tq_config["expire"]?:null);
+            }
+        }
+        if(!$call_config){
+            $this->error("电话参数获取失败!");
+        }
+        $this->assign("call_config",$call_config);
+        return view();
+    }
+
+    public function webservice_test(){
+        $url="http://webservice.agent.tq.cn/Servers/services/ServerNew?wsdl";
+        $client = new \SoapClient($url);
+        $aryResult = $client->__getFunctions();
+        var_dump($aryResult);//打印结果
+//        $aryResult = $client->getUserUinByStrid("sdzhcs1","9796221",md5("123456"));
+//        var_dump($aryResult);//打印结果
+    }
+
+    public function tq_webservice(){
+        $result = ['status'=>0 ,'info'=>"查询客户信息时发生错误！"];
+        $func_name = input("func_name","","string");
+        $params = input("params/a","","string");
+        if(!$func_name){
+            $result["info"] = "参数错误!";
+            return json($result);
+        }
+        if(empty($params)){
+            $result["info"] = "参数错误!";
+            return json($result);
+        }
+        //$params = ["sdzhcs1","9796221",md5("123456")];//e10adc3949ba59abbe56e057f20f883e
+        //var_exp($func_name,'$func_name');
+        //var_exp($params,'$params',1);
+        $url="http://webservice.agent.tq.cn/Servers/services/ServerNew?wsdl";
+        $client = new \SoapClient($url);
+        $aryResult = call_user_func_array([$client,$func_name],$params);
+        $result["status"] = 1;
+        $result["info"] = "执行成功!";
+        $result["data"] = $aryResult;
+        return json($result);
+    }
+}
