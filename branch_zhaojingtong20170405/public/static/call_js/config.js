54483e83c495186da1bae88c96238c7d5239f7eb
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-11-21 07:02:27 +0000

    更新代码自主干

--- branch_zhaojingtong20170405/public/static/call_js/config.js
+++ branch_zhaojingtong20170405/public/static/call_js/config.js
@@ -1,10 +1,10 @@
 console.log(data);
 var initOptions = {
     debug: true,
-    strid: 'sdzhcs2',
-    uin: '9796249',
-    admin_uin: '9796221',
-    appid: '4b08a3d3-8bde-4d96-b467-c41bcd08c552',
+    strid: data.strid,
+    uin: data.uin,
+    admin_uin: data.admin_uin,
+    appid: data.appid,//'4b08a3d3-8bde-4d96-b467-c41bcd08c552',
     access_token: data.access_token,
     // access_token: '2F99F6BE843D922D1408BFD878117A6F04F244B33C93ED5D04AA266CE935DA15808E8A32581F3EDD0AAA8EA5577B5C72',
     server_url: 'http://vip.agent.tq.cn',
