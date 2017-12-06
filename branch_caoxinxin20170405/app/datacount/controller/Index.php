3a8aec6c67d752678d07c56cfb9336473933fb48
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-12-06 09:34:53 +0000

    关键数据统计,添加统计表

--- branch_caoxinxin20170405/app/datacount/controller/Index.php
+++ branch_caoxinxin20170405/app/datacount/controller/Index.php
@@ -11,6 +11,8 @@ namespace app\datacount\controller;
 use app\common\controller\Initialize;
 use app\common\model\StructureEmployee;
 use app\common\model\EmployeeScore;
+use app\datacount\model\Datacount;
+use \myvendor\TimeTools;
 
 class Index extends Initialize{
     public function __construct(){
@@ -62,11 +64,159 @@ class Index extends Initialize{
 //        var_exp($per,'$per');
         $this->assign("score",$score);
         $this->assign("score_per",$per);
-//        $role_flg = check_auth("datacount/index/summary");
-//        var_exp($role_flg,'$role_flg',1);
-//        if(!$role_flg){
-//            $this->error("没有权限查看数据简报!");
-//        }
+
+        $type = 0;
+        $time = 0;
+        $data_count = $this->_get_data_count($type,$time);
+        if($data_count["status"]!=1){
+            $result['status'] = $data_count["status"];
+            $result['info'] = $data_count["info"];
+            return json($result);
+        }
+        $this->assign("data_count",$data_count["data"]);
+
+        $start_time = 0;
+        $end_time = 0;
+        $data_overview = $this->_get_data_overview($start_time,$end_time);
+        if($data_overview["status"]!=1){
+            $result['status'] = $data_overview["status"];
+            $result['info'] = $data_overview["info"];
+            return json($result);
+        }
+        $this->assign("data_overview",$data_overview["data"]);
+
+        $class_num = 0;
+        $this->assign("class_num",$class_num);
+
+        $data_num = 0;
+        $this->assign("data_num",$data_num);
         return view();
+    }
+    public function data_count(){
+        $result = ['status'=>0 ,'info'=>"获取数据关键指标时发生错误！"];
+        $type = input("type",0,"int");
+        $time = input("time",0,"int");
+        //TODO $type 值和对应权限校验
+        $data_count = $this->_get_data_count($type,$time);
+        if($data_count["status"]!=1){
+            $result['status'] = $data_count["status"];
+            $result['info'] = $data_count["info"];
+            return json($result);
+        }
+        $result['status'] = 1;
+        $result['info'] = "获取数据关键指标成功！";
+        $result['data'] = $data_count["data"];
+        return json($result);
+    }
+    public function _get_data_count($type,$time){
+        $result = ['status'=>0 ,'info'=>"获取数据关键指标时发生错误！"];
+        $result_data = [];
+        $uids = [];
+        switch ($type){
+            case 0:
+                $uids[] = $this->uid;
+                break;
+            case 1:
+                $uids[] = $this->uid;
+                break;
+        }
+        $start_time = 0;
+        $end_time = 0;
+        $timetools = new TimeTools();
+        $time_arr=[0,0];
+        switch ($time){
+            case 0:
+                $time_arr = $timetools->today();
+                break;
+            case -1:
+                $time_arr = $timetools->yesterday();
+                break;
+            case 2:
+                $time_arr = $timetools->week();
+                break;
+            case 3:
+                $time_arr = $timetools->month();
+                break;
+            case 4:
+                $time_arr = $timetools->season();
+                break;
+            case 5:
+                $time_arr = $timetools->year();
+                break;
+        }
+        if(isset($time_arr[0])){
+            $start_time = $time_arr[0];
+        }
+        if(isset($time_arr[1])){
+            $end_time = $time_arr[1];
+        }
+
+        $result_data = [
+            "new_customer"=>0,
+            "all_call_time"=>0,
+            "valid_call_time"=>0,
+            "new_sale_chance"=>0,
+            "new_sign_in"=>0,
+            "new_sale_order"=>0,
+            "new_order_money"=>0,
+        ];
+        if(empty($uids)){
+            return $result_data;
+        }
+
+        $datacountM = new Datacount();
+        $datacount = $datacountM->getDataCount($uids,$start_time,$end_time);
+//        var_exp($datacount,'$datacount',1);
+        if(isset($datacount["1"])){
+            $result_data["all_call_time"] = $datacount["1"]["all_num"]?:0;
+            $result_data["valid_call_time"] = $datacount["1"]["tag_num"]?:0;
+        }
+        if(isset($datacount["2"])){
+            $result_data["new_sale_chance"] = $datacount["2"]["all_num"];
+        }
+        if(isset($datacount["3"])){
+            $result_data["new_sale_chance"] = $datacount["3"]["all_num"];
+        }
+        if(isset($datacount["4"])){
+            $result_data["new_sale_order"] = $datacount["4"]["all_num"];
+        }
+        if(isset($datacount["5"])){
+            $result_data["new_sign_in"] = $datacount["5"]["all_num"];
+        }
+        if(isset($datacount["6"])){
+            $result_data["new_customer"] = $datacount["6"]["all_num"];
+        }
+
+        $result['status'] = 1;
+        $result['info'] = "获取数据关键指标成功！";
+        $result['data'] = $result_data;
+        return $result;
+    }
+    public function data_overview(){
+        $result = ['status'=>0 ,'info'=>"获取数据概览时发生错误！"];
+        $start_time = input("start_time",0,"int");
+        $end_time = input("end_time",0,"int");
+        //TODO $type 值和对应权限校验
+        $data_overview = $this->_get_data_overview($start_time,$end_time);
+        if($data_overview["status"]!=1){
+            $result['status'] = $data_overview["status"];
+            $result['info'] = $data_overview["info"];
+            return json($result);
+        }
+        $result['status'] = 1;
+        $result['info'] = "获取数据概览成功！";
+        $result['data'] = $data_overview["data"];
+        return json($result);
+    }
+    public function _get_data_overview($start_time,$end_time){
+        $result = ['status'=>0 ,'info'=>"获取数据概览时发生错误！"];
+        $result_data = [];
+
+
+
+        $result['status'] = 1;
+        $result['info'] = "获取数据概览成功！";
+        $result['data'] = $result_data;
+        return $result;
     }
 }
