a4e83af7cfe4aa6bcb4a8cd58a11dea62a55d24f
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-12-16 03:22:25 +0000

    更新代码自主干

--- branch_zhaojingtong20170405/app/crm/controller/Customer.php
+++ branch_zhaojingtong20170405/app/crm/controller/Customer.php
@@ -937,6 +937,10 @@ class Customer extends Initialize{
         return json($result);
     }
     public function take_customers_to_self(){
+        if(!($this->checkRule('crm/customer/public_customer_pool/take_customers'))){
+            $result=$this->noRole();
+            return json($result);
+        }
         $result = ['status'=>0 ,'info'=>"申领客户时发生错误！"];
         $ids = input('ids/a');
         if(!$ids){
@@ -962,6 +966,10 @@ class Customer extends Initialize{
         return json($result);
     }
     public function release_customers(){
+        if(!($this->checkRule('crm/customer/my_customer/release_customers'))){
+            $result=$this->noRole();
+            return json($result);
+        }
         $result = ['status'=>0 ,'info'=>"释放客户时发生错误！"];
         $ids = input('ids/a');
         if(!$ids){
@@ -987,6 +995,11 @@ class Customer extends Initialize{
     }
     public function imposed_release_customers(){
         //TODO 权限验证?
+        if(!($this->checkRule('crm/customer/customer_manage/imposed_release_customers'))){
+            $result=$this->noRole();
+            return json($result);
+        }
+
         $result = ['status'=>0 ,'info'=>"强制释放客户时发生错误！"];
         $ids = input('ids/a');
         if(!$ids){
@@ -1014,6 +1027,10 @@ class Customer extends Initialize{
     }
     public function change_customers_to_employee(){
         //TODO 权限验证?
+        if(($this->checkRule('crm/customer/customer_manage/change_customers_to_employee'))){
+            $result=$this->noRole();
+            return json($result);
+        }
         $result = ['status'=>0 ,'info'=>"重分客户时发生错误！"];
         $ids = input('ids/a');
         $uid = input('uid',0,"int");
@@ -1051,6 +1068,10 @@ class Customer extends Initialize{
     }
     public function change_customers_visible_range(){
         //TODO 权限验证?
+        if(!($this->checkRule('crm/customer/customer_manage/change_customers_visible_range'))){
+            $result=$this->noRole();
+            return json($result);
+        }
         $result = ['status'=>0 ,'info'=>"更改客户可见范围失败！"];
         $ids = input('ids/a');
         //var_exp($ids,'$ids',1);
@@ -1569,7 +1590,7 @@ class Customer extends Initialize{
     public function del(){
         if((!$this->checkRule('crm/customer/customer_manage/exportCustomer'))){
             $result=$this->noRole();
-            return $result;
+            return json($result);
         }
         $result = ['status'=>0 ,'info'=>"删除客户信息时发生错误！"];
         $ids = input('ids/a');
