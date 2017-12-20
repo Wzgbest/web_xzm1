fca75ace305d65eb6d5fd16fca9f27f5ff02edfa
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-12-20 02:29:55 +0000

    合并代码自cxx

---
+++ trunk/app/task/model/DayTask.php
@@ -0,0 +1,43 @@
+<?php
+// +----------------------------------------------------------------------
+// | 中迅传媒 [ 纯粹、极致到零 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017 http://www.baidusd.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: wuzhenguo <wuzhenguook@gmail.com> <The Best Word!!!>
+// +----------------------------------------------------------------------
+namespace app\Task\model;
+
+use app\common\model\Base;
+use think\Db;
+
+class DayTask extends Base{
+	protected $dbprefix;
+    public function __construct($corp_id =null){
+        $this->dbprefix = config('database.prefix');
+        $this->table=config('database.prefix').'employee_task';
+        parent::__construct($corp_id);
+    }
+    public function getAllDayTaskByEmployeeIds($employee_ids,$start_time,$end_time){
+        $field = [
+            "et.id",
+            "ettg.target_type",
+            "ettg.target_num",
+        ];
+        $map["et.task_type"] = 4;
+        $map["et.task_start_time"] = ["eq",$start_time];
+        $map["et.task_end_time"] = ["eq",$end_time];
+        $map["ettk.take_employee"] = ["in",$employee_ids];
+        $order = "et.id asc";
+        $standardTaskList = $this->model->table($this->table)->alias('et')
+            ->join($this->dbprefix.'employee_task_take ettk',"ettk.task_id = et.id","LEFT")
+            ->join($this->dbprefix.'employee_task_target ettg','ettg.task_id = et.id',"LEFT")
+            ->where($map)
+            ->order($order)
+            ->field($field)
+            ->group('et.id,ettg.id')
+            ->select();
+        //var_exp($standardTaskList,'$standardTaskList',1);
+        return $standardTaskList;
+    }
+}
