2692eb691bdb5007a96045b83582f20866768273
caoxinxin <caoxinxin@6de7193e-2de8-421a-a7b7-90fd0d970c29>
2017-12-21 02:29:35 +0000

    员工每日任务

--- branch_caoxinxin20170405/app/task/controller/MonthTask.php
+++ branch_caoxinxin20170405/app/task/controller/MonthTask.php
@@ -35,4 +35,115 @@ class MonthTask extends Initialize{
     }
     public function index(){
     }
+    protected function _getTaskForInput($uid){
+        $task_time = input("task_time",0,"int");
+//        var_exp($task_time,'$task_time',1);
+        $task_info['task_name'] = input("task_name","","string");
+        $task_info['task_start_time'] = mktime(0, 0, 0, date('m',$task_time), 1, date('Y',$task_time));
+        $task_info['task_end_time'] = mktime(23, 59, 59, date('m',$task_time), date('t',$task_time), date('Y',$task_time));
+        $task_info['task_take_start_time'] = $task_info['task_start_time'];
+        $task_info['task_take_end_time'] = $task_info['task_end_time'];
+        $task_info['task_type'] = 4;
+        $task_info['task_method'] = 6;
+        $task_info['content'] = input("content","","string");
+
+        $task_info['public_to_take'] = input("public_to_take","","string");
+        $public_uids = explode(",",$task_info['public_to_take']);
+        $public_uids = array_filter($public_uids);
+        $public_uids = array_unique($public_uids);
+        $task_info['public_to_take'] = implode(",",$public_uids);
+        $task_info['public_to_view'] = $task_info['public_to_take'];
+
+        $task_info['create_employee'] = $uid;
+        $task_info['create_time'] = time();
+        $task_info['status'] = 2;
+        return $task_info;
+    }
+    protected function _getTaskTargetForInput(){
+        $task_target_infos = [];
+        $task_target_str = input("target");
+        $task_target_arr = json_decode($task_target_str,true);
+        //var_exp($task_target_arr,'$task_target_arr',1);
+
+        foreach ($task_target_arr as $task_target){
+            if(!isset($task_target['target_type'])||!isset($task_target['target_num'])){
+                return [];
+            }
+            $task_target_info['target_type'] = $task_target['target_type'];
+            $task_target_info['target_num'] = $task_target['target_num'];
+            $task_target_info['target_method'] = 0;
+
+            //var_exp($task_target_info,'$task_target_info',1);
+            $task_target_infos[] = $task_target_info;
+        }
+        return $task_target_infos;
+    }
+    public function add(){
+        $result = ['status'=>0 ,'info'=>"新建月度任务时发生错误！"];
+        $userinfo = get_userinfo();
+        $uid = $userinfo["userid"];
+        $time = time();
+        $taskInfo = $this->_getTaskForInput($uid);
+        $taskInfo["reward_max_num"] = 0;
+        $taskTargetInfos = $this->_getTaskTargetForInput();
+//        var_exp($taskTargetInfos,'$taskTargetInfos',1);
+        if(empty($taskTargetInfos)){
+            $result['info'] = '任务目标参数错误';
+            return json($result);
+        }
+        $taskTakeInfos = [];
+        $public_uids = explode(",",$taskInfo["public_to_take"]);
+        $public_uids = array_filter($public_uids);
+        $public_uids = array_unique($public_uids);
+        foreach ($public_uids as $employee_id){
+            $taskTakeInfos[] = [
+                "take_employee"=>$employee_id,
+                "take_time"=>$time
+            ];
+        }
+        if(empty($taskTakeInfos)){
+            $result['info'] = '任务对象参数错误';
+            return json($result);
+        }
+
+        $employeeTaskM = new EmployeeTaskModel($this->corp_id);
+        $taskTargetM = new TaskTargetModel($this->corp_id);
+        try {
+            $employeeTaskM->link->startTrans();
+
+            $taskId = $employeeTaskM->addTask($taskInfo);
+            if (!$taskId) {
+                exception('提交任务失败!');
+            }
+
+            foreach ($taskTargetInfos as &$taskTargetInfo) {
+                $taskTargetInfo['task_id'] = $taskId;
+            }
+            $taskTargetId = $taskTargetM->addMutipleTaskTarget($taskTargetInfos);
+            if (!$taskTargetId) {
+                exception('提交任务目标失败!');
+            }
+
+            foreach ($taskTakeInfos as &$taskTakeInfo) {
+                $taskTakeInfo['task_id'] = $taskId;
+            }
+            $taskTakeM = new TaskTakeModel($this->corp_id);
+            $taskTakeId = $taskTakeM->addMutipleTaskTake($taskTakeInfos);
+            if(!$taskTakeId){
+                exception('提交任务参与信息失败!');
+            }
+
+            $employeeTaskM->link->commit();
+            $result['data'] = $taskId;
+        }catch (\Exception $ex){
+            $employeeTaskM->link->rollback();
+            //print_r($ex->getTrace());die();
+            $result['info'] = $ex->getMessage();
+            return json($result);
+        }
+
+        $result['status'] = 1;
+        $result['info'] = "新建任务成功！";
+        return json($result);
+    }
 }
